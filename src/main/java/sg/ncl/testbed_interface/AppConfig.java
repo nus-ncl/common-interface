c8cbd030e3b8a8bfa5ba072c3e1ff496814141d8
Christopher Zhong <ChrisZhong@users.noreply.github.com>
2016-04-13 11:58:17 +0800

    Add REST API for version (#2)

---
+++ src/main/java/sg/ncl/testbed_interface/AppConfig.java
@@ -0,0 +1,21 @@
+package sg.ncl.testbed_interface;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author Christopher Zhong
+ */
+@Configuration
+public class AppConfig {
+
+    @Bean
+    @Scope("singleton")
+    VersionInfo versionInfo() {
+        return new VersionInfo(1,0,"3ade5", LocalDateTime.now());
+    }
+
+}
---
+++ src/main/java/sg/ncl/testbed_interface/controllers/VersionController.java
@@ -0,0 +1,30 @@
+package sg.ncl.testbed_interface.controllers;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import sg.ncl.testbed_interface.VersionInfo;
+
+import javax.inject.Inject;
+
+/**
+ * @author Christopher Zhong
+ */
+@RestController()
+@RequestMapping(path = "/version")
+public class VersionController {
+
+    private final VersionInfo versionInfo;
+
+    @Inject
+    protected VersionController(final VersionInfo versionInfo) {
+        this.versionInfo = versionInfo;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public VersionInfo version() {
+        return versionInfo;
+    }
+
+}
