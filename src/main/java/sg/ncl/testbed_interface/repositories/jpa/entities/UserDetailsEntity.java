c601c409da9ca78f4f4c487b8fd9db7a23ff79a3
Christopher Zhong <ChrisZhong@users.noreply.github.com>
2016-04-25 16:34:50 +0800

    Change SQL field name (DEV-15)

--- src/main/java/sg/ncl/testbed_interface/repositories/jpa/entities/UserDetailsEntity.java
+++ src/main/java/sg/ncl/testbed_interface/repositories/jpa/entities/UserDetailsEntity.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
  * @author Christopher Zhong
  */
 @Entity
-@Table(name = "personal_details")
+@Table(name = "user_details")
 public class UserDetailsEntity extends AbstractEntity implements UserDetails {
 
     @Id
