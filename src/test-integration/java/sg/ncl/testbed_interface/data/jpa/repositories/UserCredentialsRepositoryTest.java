db2efbff983dead434ea7abeb70729849c1591bd
Christopher Zhong <ChrisZhong@users.noreply.github.com>
2016-05-10 13:49:13 +0800

    Extract superclass for integration testing (DEV-84)

---
+++ src/test-integration/java/sg/ncl/testbed_interface/data/jpa/repositories/UserCredentialsRepositoryTest.java
@@ -0,0 +1,18 @@
+package sg.ncl.testbed_interface.data.jpa.repositories;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author Christopher Zhong
+ */
+
+public class UserCredentialsRepositoryTest extends AbstractTest {
+
+    @Test
+    public void test() throws Exception {
+
+    }
+
+}
