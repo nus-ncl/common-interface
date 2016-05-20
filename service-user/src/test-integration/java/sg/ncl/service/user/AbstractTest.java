82f303d24806bb1012ff1dbe3c3c24a6f70ddc0a
Christopher Zhong <ChrisZhong@users.noreply.github.com>
2016-05-20 19:54:13 +0800

    Enable transactions in integration test (DEV-100)

--- service-user/src/test-integration/java/sg/ncl/service/user/AbstractTest.java
+++ service-user/src/test-integration/java/sg/ncl/service/user/AbstractTest.java
@@ -2,13 +2,15 @@ package sg.ncl.service.user;
 
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 /**
  * @author Christopher Zhong
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = TestConfig.class)
-public abstract class AbstractTest extends AbstractJUnit4SpringContextTests {
+@SpringApplicationConfiguration(classes = {App.class, TestConfig.class})
+@WebAppConfiguration
+public abstract class AbstractTest extends AbstractTransactionalJUnit4SpringContextTests {
 }
