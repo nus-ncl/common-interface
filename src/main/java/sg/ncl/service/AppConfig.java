13664bf72b50ab96c106585839adff4402126ce4
Christopher Zhong <chriszhong@users.noreply.github.com>
2016-05-21 22:36:56 +0800

    Add missing services to root project (DEV-100)

--- src/main/java/sg/ncl/service/AppConfig.java
+++ src/main/java/sg/ncl/service/AppConfig.java
@@ -2,11 +2,17 @@ package sg.ncl.service;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import sg.ncl.service.experiment.*;
 
 /**
  * @author Christopher Zhong
  */
 @Configuration("sg.ncl.service.AppConfig")
-@Import({sg.ncl.service.user.AppConfig.class, sg.ncl.service.team.AppConfig.class, sg.ncl.service.version.AppConfig.class})
+@Import({sg.ncl.service.authentication.AppConfig.class,
+        sg.ncl.service.experiment.AppConfig.class,
+        sg.ncl.service.realization.AppConfig.class,
+        sg.ncl.service.user.AppConfig.class,
+        sg.ncl.service.team.AppConfig.class,
+        sg.ncl.service.version.AppConfig.class})
 public class AppConfig {
 }
