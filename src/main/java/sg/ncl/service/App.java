fd18594ea2fd466ae3b20f43b269c158d2cda7b0
Christopher Zhong <ChrisZhong@users.noreply.github.com>
2016-05-23 17:31:45 +0800

    Temporarily remove Spring Security (DEV-99)

--- src/main/java/sg/ncl/service/App.java
+++ src/main/java/sg/ncl/service/App.java
@@ -2,13 +2,12 @@ package sg.ncl.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 /**
  * @author Christopher Zhong
  */
-@SpringBootApplication(exclude = {
-        sg.ncl.service.authentication.SecurityConfig.class,
-        sg.ncl.service.user.SecurityConfig.class})
+@SpringBootApplication(exclude = {EnableWebSecurity.class, SpringBootApplication.class})
 public class App {
 
     public static void main(String[] args) {
