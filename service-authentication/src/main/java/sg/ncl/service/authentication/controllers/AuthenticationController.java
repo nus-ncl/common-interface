133b0b3fd4cd9ae7cb2b4b05b7e0ec8c03e16fd9
Christopher Zhong <ChrisZhong@users.noreply.github.com>
2016-05-24 16:15:30 +0800

    Add decoding for base64 authorization header (DEV-100)

--- service-authentication/src/main/java/sg/ncl/service/authentication/controllers/AuthenticationController.java
+++ service-authentication/src/main/java/sg/ncl/service/authentication/controllers/AuthenticationController.java
@@ -1,6 +1,10 @@
 package sg.ncl.service.authentication.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.util.Base64Utils;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,6 +21,8 @@ import javax.inject.Inject;
 @RequestMapping(path = "/authentication", produces = MediaType.APPLICATION_JSON_VALUE)
 public class AuthenticationController {
 
+    private static final Logger logger = LoggerFactory.getLogger(AuthenticationController.class);
+
     private final AuthenticationService authenticationService;
 
     @Inject
@@ -25,11 +31,25 @@ public class AuthenticationController {
     }
 
     @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public String login(@RequestHeader("X-Username") String username, @RequestHeader("X-Password") String password) {
-        if (username.isEmpty() || password.isEmpty()) {
+    public String login(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorization) {
+        if (authorization.isEmpty()) {
+            logger.warn("Empty authorization header");
             throw new BadRequestException();
         }
-        return authenticationService.login(username, password);
+        if (authorization.startsWith("Basic ")) {
+            final byte[] bytes = Base64Utils.decodeFromString(authorization.substring(6));
+            final String decoded = new String(bytes);
+            final String[] split = decoded.split(":");
+            if (split.length == 2) {
+                final String username = split[0];
+                final String password = split[1];
+                return authenticationService.login(username, password);
+            }
+            logger.warn("Invalid basic authentication");
+            throw new BadRequestException();
+        }
+        logger.warn("Unknown authorization scheme");
+        throw new BadRequestException();
     }
 
     @RequestMapping(method = RequestMethod.DELETE)
