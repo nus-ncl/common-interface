14c7a2517a4f7edbf8fd1682ab1e56b58d485f1f
Christopher Zhong <ChrisZhong@users.noreply.github.com>
2016-05-24 11:37:32 +0800

    Remove leftover SecurityConfig (DEV-99)

--- src/main/java/sg/ncl/service/App.java
+++ src/main/java/sg/ncl/service/App.java
@@ -2,12 +2,11 @@ package sg.ncl.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 /**
  * @author Christopher Zhong
  */
-@SpringBootApplication(exclude = {EnableWebSecurity.class, SpringBootApplication.class})
+@SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
