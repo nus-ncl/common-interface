5a7cb9aafd74da22c4beb7c622ca227321cfef72
Christopher Zhong <ChrisZhong@users.noreply.github.com>
2016-05-26 10:53:55 +0800

    Fix bug with Jpa error when running all services together (DEV-99)

--- src/main/java/sg/ncl/service/App.java
+++ src/main/java/sg/ncl/service/App.java
@@ -2,11 +2,13 @@ package sg.ncl.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import sg.ncl.common.jpa.UseJpa;
 
 /**
  * @author Christopher Zhong
  */
 @SpringBootApplication
+@UseJpa
 public class App {
 
     public static void main(String[] args) {
