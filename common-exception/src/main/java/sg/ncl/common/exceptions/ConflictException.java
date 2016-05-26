abef519326aef5227cb1278ebe834bf9c1b1fa87
Christopher Zhong <ChrisZhong@users.noreply.github.com>
2016-05-26 23:27:58 +0800

    Update exceptions to expose constructors from `RuntimeException` (DEV-99)

--- common-exception/src/main/java/sg/ncl/common/exceptions/ConflictException.java
+++ common-exception/src/main/java/sg/ncl/common/exceptions/ConflictException.java
@@ -7,4 +7,24 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  * @author Christopher Zhong
  */
 @ResponseStatus(HttpStatus.CONFLICT)
-public class ConflictException extends RuntimeException {}
+public class ConflictException extends RuntimeException {
+
+    public ConflictException() {}
+
+    public ConflictException(final String message) {
+        super(message);
+    }
+
+    public ConflictException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    public ConflictException(final Throwable cause) {
+        super(cause);
+    }
+
+    protected ConflictException(final String message, final Throwable cause, final boolean enableSuppression, final boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+}
