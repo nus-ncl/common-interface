b231b458f8e064b3567d7c1b9b1cf98d1784f4f3
desmondlim <desmond.lim@nus.edu.sg>
2016-05-27 08:22:44 +0800

    Add getAllUserTest.

--- service-user/src/test-integration/java/sg/ncl/service/user/UserServiceTest.java
+++ service-user/src/test-integration/java/sg/ncl/service/user/UserServiceTest.java
@@ -1,11 +1,17 @@
 package sg.ncl.service.user;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.hamcrest.collection.IsIterableContainingInAnyOrder;
 import org.junit.Assert;
 import org.junit.Test;
+import sg.ncl.service.user.data.jpa.entities.AddressEntity;
+import sg.ncl.service.user.data.jpa.entities.UserDetailsEntity;
+import sg.ncl.service.user.data.jpa.entities.UserEntity;
 import sg.ncl.service.user.data.jpa.repositories.UserRepository;
 import sg.ncl.service.user.domain.User;
 
 import javax.inject.Inject;
+import java.time.ZonedDateTime;
 import java.util.List;
 
 /**
@@ -22,5 +28,47 @@ public class UserServiceTest extends AbstractTest {
         Assert.assertTrue(list.size() == 0);
     }
 
+    @Test
+    public void getAllUserTest() throws Exception {
+        UserService userService = new UserService(userRepository);
+        User[] userArray = new User[3];
 
+        for (int i = 0; i < 3; i++) {
+            UserEntity[] userEntityArray = addUser();
+            userArray[i] = userEntityArray[0];
+        }
+
+        List<User> userList2 = userService.get();
+
+        Assert.assertThat(userList2, IsIterableContainingInAnyOrder.containsInAnyOrder(userArray));
+    }
+
+    private UserEntity[] addUser() throws Exception {
+        final UserEntity userEntity = new UserEntity();
+        userEntity.setApplicationDate(ZonedDateTime.now());
+
+        final UserDetailsEntity userDetailsEntity = new UserDetailsEntity();
+        userDetailsEntity.setFirstName(RandomStringUtils.randomAlphabetic(20));
+        userDetailsEntity.setLastName(RandomStringUtils.randomAlphabetic(20));
+        userDetailsEntity.setEmail(RandomStringUtils.randomAlphabetic(20));
+        userDetailsEntity.setPhone(RandomStringUtils.randomAlphabetic(20));
+
+        final AddressEntity address = new AddressEntity();
+        address.setAddress1(RandomStringUtils.randomAlphabetic(20));
+        address.setAddress2(RandomStringUtils.randomAlphabetic(20));
+        address.setCountry(RandomStringUtils.randomAlphabetic(20));
+        address.setRegion(RandomStringUtils.randomAlphabetic(20));
+        address.setZipCode(RandomStringUtils.randomAlphabetic(20));
+
+        userDetailsEntity.setAddress(address);
+        userEntity.setUserDetails(userDetailsEntity);
+
+        UserEntity saveUser = userRepository.save(userEntity);
+
+        final UserEntity[] userArray = new UserEntity[2];
+        userArray[0] = saveUser;
+        userArray[1] = userEntity;
+
+        return userArray;
+    }
 }
