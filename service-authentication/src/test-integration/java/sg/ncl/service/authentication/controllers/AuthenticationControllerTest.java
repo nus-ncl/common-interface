c5b98945b752b1ca127470dbe774e5b61f730d4e
Christopher Zhong <ChrisZhong@users.noreply.github.com>
2016-05-30 17:13:36 +0800

    Add placeholder integration tests for `AuthenticationController` and `AuthenticationService` (DEV-99)

---
+++ service-authentication/src/test-integration/java/sg/ncl/service/authentication/controllers/AuthenticationControllerTest.java
@@ -0,0 +1,36 @@
+package sg.ncl.service.authentication.controllers;
+
+import org.junit.Test;
+import sg.ncl.service.authentication.AbstractTest;
+
+/**
+ * @author Christopher Zhong
+ */
+public class AuthenticationControllerTest extends AbstractTest {
+
+    @Test
+    public void test1() {
+        // TODO test the login with a good case
+    }
+
+    @Test
+    public void test2() {
+        // TODO test the login with a missing authorization header
+    }
+
+    @Test
+    public void test3() {
+        // TODO test the login with an empty authorization header
+    }
+
+    @Test
+    public void test4() {
+        // TODO test the login with an invalid basic authentication
+    }
+
+    @Test
+    public void test5() {
+        // TODO test the login with an unknown authorization scheme
+    }
+
+}
---
+++ service-authentication/src/test-integration/java/sg/ncl/service/authentication/services/AuthenticationServiceTest.java
@@ -0,0 +1,16 @@
+package sg.ncl.service.authentication.services;
+
+import org.junit.Test;
+import sg.ncl.service.authentication.AbstractTest;
+
+/**
+ * @author Christopher Zhong
+ */
+public class AuthenticationServiceTest extends AbstractTest {
+
+    @Test
+    public void test1() {
+        // TODO test login method with a good case
+    }
+
+}
