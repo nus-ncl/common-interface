48afc453201fddbfd7bcc9254d1798663a38b028
Christopher Zhong <ChrisZhong@users.noreply.github.com>
2016-05-31 13:23:57 +0800

    Migrate `service-user` project to use the new `common-jpa` syntax (DEV-99)

---
+++ service-user/src/main/java/sg/ncl/service/user/AppConfig.java
@@ -0,0 +1,13 @@
+package sg.ncl.service.user;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import sg.ncl.common.jpa.JpaConfig;
+import sg.ncl.common.jwt.JwtConfig;
+
+/**
+ * @author Christopher Zhong
+ */
+@Configuration("sg.ncl.service.user.AppConfig")
+@Import({JpaConfig.class, JwtConfig.class})
+public class AppConfig {}
