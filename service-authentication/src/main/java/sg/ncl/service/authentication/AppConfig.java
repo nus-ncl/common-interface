1440c3feee347104ace7dd85e94b2256b2924637
Christopher Zhong <ChrisZhong@users.noreply.github.com>
2016-05-31 13:22:09 +0800

    Migrate `service-authentication` project to use the new `common-jpa` syntax (DEV-99)

--- service-authentication/src/main/java/sg/ncl/service/authentication/AppConfig.java
+++ service-authentication/src/main/java/sg/ncl/service/authentication/AppConfig.java
@@ -2,11 +2,12 @@ package sg.ncl.service.authentication;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import sg.ncl.common.jpa.JpaConfig;
 import sg.ncl.common.jwt.JwtConfig;
 
 /**
  * @author Christopher Zhong
  */
 @Configuration("sg.ncl.service.authentication.AppConfig")
-@Import({JwtConfig.class})
+@Import({JpaConfig.class, JwtConfig.class})
 public class AppConfig {}
