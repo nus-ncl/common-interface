90dca6711b46906045150b55401f5cb6ea4fa8ff
Christopher Zhong <ChrisZhong@users.noreply.github.com>
2016-05-31 14:30:20 +0800

    DEV-249 Implement `addCredentials`

--- service-authentication/src/main/java/sg/ncl/service/authentication/services/CredentialsService.java
+++ service-authentication/src/main/java/sg/ncl/service/authentication/services/CredentialsService.java
@@ -1,5 +1,8 @@
 package sg.ncl.service.authentication.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import sg.ncl.common.exceptions.BadRequestException;
@@ -8,6 +11,9 @@ import sg.ncl.service.authentication.data.jpa.repositories.CredentialsRepository
 import sg.ncl.service.authentication.domain.Credentials;
 import sg.ncl.service.authentication.domain.CredentialsStatus;
 import sg.ncl.service.authentication.exceptions.CredentialsNotFoundException;
+import sg.ncl.service.authentication.exceptions.NullPasswordException;
+import sg.ncl.service.authentication.exceptions.NullUserIdException;
+import sg.ncl.service.authentication.exceptions.NullUsernameException;
 import sg.ncl.service.authentication.exceptions.UserIdAlreadyExistsException;
 import sg.ncl.service.authentication.exceptions.UsernameAlreadyExistsException;
 
@@ -20,11 +26,15 @@ import java.util.List;
 @Service
 public class CredentialsService {
 
+    private static final Logger logger = LoggerFactory.getLogger(CredentialsService.class);
+
     private final CredentialsRepository credentialsRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Inject
-    protected CredentialsService(final CredentialsRepository credentialsRepository) {
+    protected CredentialsService(final CredentialsRepository credentialsRepository, final PasswordEncoder passwordEncoder) {
         this.credentialsRepository = credentialsRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional(readOnly = true)
@@ -34,23 +44,35 @@ public class CredentialsService {
 
     @Transactional
     public void addCredentials(final Credentials credentials) {
-        if (credentials.getUsername() == null || credentials.getPassword() == null || credentials.getUserId() == null) {
-            throw new BadRequestException();
+        if (credentials.getUsername() == null) {
+            logger.warn("Username is null");
+            throw new NullUsernameException();
+        }
+        if (credentials.getPassword() == null) {
+            logger.warn("Password is null");
+            throw new NullPasswordException();
+        }
+        if (credentials.getUserId() == null) {
+            logger.warn("User ID is null");
+            throw new NullUserIdException();
         }
         // check if the username already exists
         if (credentialsRepository.findByUsername(credentials.getUsername()) == null) {
             // check if the user id already exists
             if (credentialsRepository.findByUserId(credentials.getUserId()) == null) {
-                final CredentialsEntity entity = new CredentialsEntity();
+                CredentialsEntity entity = new CredentialsEntity();
                 entity.setUsername(credentials.getUsername());
                 setPassword(entity, credentials.getPassword());
                 entity.setUserId(credentials.getUserId());
                 entity.setStatus(CredentialsStatus.ACTIVE);
-                credentialsRepository.save(entity);
+                entity = credentialsRepository.save(entity);
+                logger.info("New credentials `{}`", entity);
             }
-            throw new UserIdAlreadyExistsException();
+            logger.warn("User ID `{}` is already associated with a credentials", credentials.getUserId());
+            throw new UserIdAlreadyExistsException(credentials.getUserId());
         }
-        throw new UsernameAlreadyExistsException();
+        logger.warn("Username `{}` is already associated with a credentials", credentials.getUsername());
+        throw new UsernameAlreadyExistsException(credentials.getUsername());
     }
 
     @Transactional
@@ -66,9 +88,8 @@ public class CredentialsService {
         setPassword(entity, credentials.getPassword());
     }
 
-    private void setPassword(CredentialsEntity entity, String password) {
-        // FIXME use spring security bccrypt
-        entity.setPassword(password);
+    private void setPassword(final CredentialsEntity entity, final String password) {
+        entity.setPassword(passwordEncoder.encode(password));
     }
 
 }
