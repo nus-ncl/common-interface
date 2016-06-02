91a90d85d105586fcfab486e56eb9ea124009f8c
Christopher Zhong <ChrisZhong@users.noreply.github.com>
2016-06-02 13:32:03 +0800

    DEV-99 Add new exceptions instead of the same `BadRequestException`

---
+++ service-authentication/src/main/java/sg/ncl/service/authentication/exceptions/EmptyAuthorizationHeaderException.java
@@ -0,0 +1,28 @@
+package sg.ncl.service.authentication.exceptions;
+
+import sg.ncl.common.exceptions.BadRequestException;
+
+/**
+ * @author Christopher Zhong
+ */
+public class EmptyAuthorizationHeaderException extends BadRequestException {
+
+    public EmptyAuthorizationHeaderException() {}
+
+    public EmptyAuthorizationHeaderException(final String message) {
+        super(message);
+    }
+
+    public EmptyAuthorizationHeaderException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    public EmptyAuthorizationHeaderException(final Throwable cause) {
+        super(cause);
+    }
+
+    protected EmptyAuthorizationHeaderException(final String message, final Throwable cause, final boolean enableSuppression, final boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+}
