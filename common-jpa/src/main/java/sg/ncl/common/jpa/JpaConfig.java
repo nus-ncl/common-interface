748e8ab3ad070ed2c12bdac3ba3f5a23c802f5d4
Christopher Zhong <ChrisZhong@users.noreply.github.com>
2016-06-04 22:41:23 +0800

    Revert "DEV-312 Migrate from @Bean to @Named annotations"

--- common-jpa/src/main/java/sg/ncl/common/jpa/JpaConfig.java
+++ common-jpa/src/main/java/sg/ncl/common/jpa/JpaConfig.java
@@ -4,10 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-import javax.inject.Named;
 import javax.sql.DataSource;
 
 /**
@@ -19,7 +19,7 @@ public class JpaConfig {
 
     private static final Logger logger = LoggerFactory.getLogger(JpaConfig.class);
 
-    @Named
+    @Bean
     public DataSource dataSource(@Value("${spring.datasource.url}") final String url, @Value("${spring.datasource.username}") final String username, @Value("${spring.datasource.password}") final String password) {
         logger.info("Connecting to `{}` as `{}`", url, username);
         return DataSourceBuilder.create().url(url).username(username).password(password).build();
