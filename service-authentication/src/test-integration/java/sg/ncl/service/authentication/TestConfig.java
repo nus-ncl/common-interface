020be85419170fd0b025d03d572862b27b897ff1
Christopher Zhong <ChrisZhong@users.noreply.github.com>
2016-06-07 12:20:52 +0800

    DEV-248 Add mock beans

--- service-authentication/src/test-integration/java/sg/ncl/service/authentication/TestConfig.java
+++ service-authentication/src/test-integration/java/sg/ncl/service/authentication/TestConfig.java
@@ -1,8 +1,12 @@
 package sg.ncl.service.authentication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import sg.ncl.service.authentication.data.jpa.repositories.CredentialsRepository;
 import sg.ncl.service.authentication.services.AuthenticationService;
 
 import static org.mockito.Mockito.mock;
@@ -13,10 +17,27 @@ import static org.mockito.Mockito.mock;
 @Configuration
 public class TestConfig {
 
+    private static final Logger logger = LoggerFactory.getLogger(TestConfig.class);
+
     @Bean
     @Profile("mock-authentication-service")
     public AuthenticationService authenticationService() {
+        logger.info("Mocking: {}", AuthenticationService.class);
         return mock(AuthenticationService.class);
+    }
+
+    @Bean
+    @Profile("mock-password-encoder")
+    public PasswordEncoder passwordEncoder() {
+        logger.info("Mocking: {}", PasswordEncoder.class);
+        return mock(PasswordEncoder.class);
+    }
+
+    @Bean
+    @Profile("mock-credentials-repository")
+    public CredentialsRepository credentialsRepository() {
+        logger.info("Mocking: {}", CredentialsRepository.class);
+        return mock(CredentialsRepository.class);
     }
 
 }
