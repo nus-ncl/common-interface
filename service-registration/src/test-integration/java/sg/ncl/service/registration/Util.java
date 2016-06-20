31dcaf8589ff1ad875c159aaf2df783fc2df42fd
teye <a0097953@gmail.com>
2016-06-20 12:17:39 +0800

    extend service-registration to accept the additional required deter form fields

--- service-registration/src/test-integration/java/sg/ncl/service/registration/Util.java
+++ service-registration/src/test-integration/java/sg/ncl/service/registration/Util.java
@@ -31,14 +31,19 @@ public class Util {
         final UserDetailsEntity userDetailsEntity = new UserDetailsEntity();
         userDetailsEntity.setFirstName(RandomStringUtils.randomAlphabetic(20));
         userDetailsEntity.setLastName(RandomStringUtils.randomAlphabetic(20));
+        userDetailsEntity.setJobTitle(RandomStringUtils.randomAlphabetic(20));
         userDetailsEntity.setEmail(RandomStringUtils.randomAlphabetic(8) + "@nus.edu.sg");
         userDetailsEntity.setPhone(RandomStringUtils.randomAlphabetic(20));
+        userDetailsEntity.setInstitution(RandomStringUtils.randomAlphabetic(20));
+        userDetailsEntity.setInstitutionAbbreviation(RandomStringUtils.randomAlphabetic(20));
+        userDetailsEntity.setInstitutionWeb(RandomStringUtils.randomAlphabetic(20));
 
         final AddressEntity address = new AddressEntity();
         address.setAddress1(RandomStringUtils.randomAlphabetic(20));
         address.setAddress2(RandomStringUtils.randomAlphabetic(20));
         address.setCountry(RandomStringUtils.randomAlphabetic(20));
         address.setRegion(RandomStringUtils.randomAlphabetic(20));
+        address.setCity(RandomStringUtils.randomAlphabetic(20));
         address.setZipCode(RandomStringUtils.randomAlphabetic(20));
 
         userDetailsEntity.setAddress(address);
@@ -69,7 +74,7 @@ public class Util {
         registrationEntity.setUsrState(RandomStringUtils.randomAlphabetic(20));
         registrationEntity.setUsrEmail(RandomStringUtils.randomAlphabetic(20));
 
-        registrationEntity.setUsrName(RandomStringUtils.randomAlphabetic(20));
+        registrationEntity.setUsrName(RandomStringUtils.randomAlphabetic(4) + " " + RandomStringUtils.randomAlphabetic(4));
         registrationEntity.setUsrPhone(RandomStringUtils.randomAlphabetic(20));
         registrationEntity.setUsrTitle(RandomStringUtils.randomAlphabetic(20));
         registrationEntity.setUsrZip(RandomStringUtils.randomAlphabetic(20));
