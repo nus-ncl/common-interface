79095f1856bb02a54f68d2e21fa4705c5f91d52b
teye <a0097953@gmail.com>
2016-06-21 17:59:27 +0800

    make users test to use Util

--- service-user/src/main/java/sg/ncl/service/user/services/UserService.java
+++ service-user/src/main/java/sg/ncl/service/user/services/UserService.java
@@ -128,8 +128,12 @@ public class UserService {
         userDetailsEntity.setLastModifiedDate(ZonedDateTime.now());
         userDetailsEntity.setFirstName("john");
         userDetailsEntity.setLastName("doe");
+        userDetailsEntity.setJobTitle("research assistant");
         userDetailsEntity.setEmail("johndoe@nus.edu.sg");
         userDetailsEntity.setPhone("12345678");
+        userDetailsEntity.setInstitution("national university of singapore");
+        userDetailsEntity.setInstitutionAbbreviation("NUS");
+        userDetailsEntity.setInstitutionWeb("http://www.nus.edu.sg");
 
         final AddressEntity address = new AddressEntity();
         address.setCreatedDate(ZonedDateTime.now());
@@ -137,6 +141,7 @@ public class UserService {
         address.setAddress1("computing drive 12");
         address.setAddress2("another address 2");
         address.setCountry("singapore");
+        address.setCity("singapore");
         address.setRegion("west");
         address.setZipCode("123456");
 
