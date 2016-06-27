3fa2b33ece97b91a00410ae5b8c2b61d84dd9697
Desmond Lim <desmond.lim@nus.edu.sg>
2016-06-27 16:19:20 +0800

    Add service for save, get all and get by user id

--- service-experiment/src/main/java/sg/ncl/service/experiment/data/jpa/ExperimentRepository.java
+++ service-experiment/src/main/java/sg/ncl/service/experiment/data/jpa/ExperimentRepository.java
@@ -2,8 +2,13 @@ package sg.ncl.service.experiment.data.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * @author Christopher Zhong
  */
 public interface ExperimentRepository extends JpaRepository<ExperimentEntity, Long> {
+
+    List<ExperimentEntity> findByUserId(String userId);
+
 }
---
+++ service-experiment/src/main/java/sg/ncl/service/experiment/logic/ExperimentService.java
@@ -0,0 +1,51 @@
+package sg.ncl.service.experiment.logic;
+
+import org.springframework.stereotype.Service;
+import sg.ncl.service.experiment.data.jpa.ExperimentEntity;
+import sg.ncl.service.experiment.data.jpa.ExperimentRepository;
+import sg.ncl.service.experiment.domain.Experiment;
+
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Desmond.
+ */
+@Service
+public class ExperimentService {
+
+    private final ExperimentRepository experimentRepository;
+
+    @Inject
+    protected ExperimentService(final ExperimentRepository experimentRepositoryRepository) {
+        this.experimentRepository = experimentRepositoryRepository;
+    }
+
+    public ExperimentEntity save(Experiment experiment) {
+        ExperimentEntity experimentEntity = new ExperimentEntity();
+
+        experimentEntity.setUserId(experiment.getUserId());
+        experimentEntity.setTeamId(experiment.getTeamId());
+        experimentEntity.setName(experiment.getName());
+        experimentEntity.setDescription(experiment.getDescription());
+        experimentEntity.setNsFile(experiment.getNsFile());
+        experimentEntity.setIdleSwap(experiment.getIdleSwap());
+        experimentEntity.setMaxDuration(experiment.getMaxDuration());
+
+        return experimentRepository.save(experimentEntity);
+    }
+
+    public List<ExperimentEntity> get() {
+        final List<ExperimentEntity> result = new ArrayList<>();
+        for (ExperimentEntity experimentEntity : experimentRepository.findAll()) {
+            result.add(experimentEntity);
+        }
+        return result;
+    }
+
+    public List<ExperimentEntity> getByUser(String userId) {
+        List<ExperimentEntity> result = experimentRepository.findByUserId(userId);
+        return result;
+    }
+}
