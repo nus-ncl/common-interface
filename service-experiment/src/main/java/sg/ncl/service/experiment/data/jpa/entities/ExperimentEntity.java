4394fa8d7dc83fe631d889ca28d2b2073bbc29d1
Desmond Lim <desmond.lim@nus.edu.sg>
2016-06-27 11:08:43 +0800

    Add toString method

--- service-experiment/src/main/java/sg/ncl/service/experiment/data/jpa/entities/ExperimentEntity.java
+++ service-experiment/src/main/java/sg/ncl/service/experiment/data/jpa/entities/ExperimentEntity.java
@@ -48,29 +48,7 @@ public class ExperimentEntity extends AbstractEntity implements Experiment {
         this.id = id;
     }
 
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) { return true; }
-        if (o == null || getClass() != o.getClass()) { return false; }
 
-        final Experiment that = (Experiment) o;
-
-        return getId() == null ? that.getId() == null : getId().equals(that.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return getId() == null ? 0 : getId().hashCode();
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("ExperimentEntity{");
-        sb.append("id='").append(id).append('\'');
-        sb.append(", super=").append(super.toString());
-        sb.append('}');
-        return sb.toString();
-    }
 
     @Override
     public String getTeamId() {
@@ -124,5 +102,35 @@ public class ExperimentEntity extends AbstractEntity implements Experiment {
 
     public void setMaxDuration(Integer maxDuration) {
         this.maxDuration = maxDuration;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) { return true; }
+        if (o == null || getClass() != o.getClass()) { return false; }
+
+        final Experiment that = (Experiment) o;
+
+        return getId() == null ? that.getId() == null : getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getId() == null ? 0 : getId().hashCode();
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("ExperimentEntity{");
+        sb.append("id='").append(id).append('\'');
+        sb.append(", teamId=").append(teamId);
+        sb.append(", name=").append(name);
+        sb.append(", description=").append(description);
+        sb.append(", nsFile=").append(nsFile);
+        sb.append(", idleSwap=").append(idleSwap);
+        sb.append(", maxDuration=").append(maxDuration);
+        sb.append(", super=").append(super.toString());
+        sb.append('}');
+        return sb.toString();
     }
 }
