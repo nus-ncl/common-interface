99b1a2bf5c9e6f6866ed840194a0d474df46a839
teye <a0097953@gmail.com>
2016-06-27 15:50:39 +0800

    create the team member info to distinguish between team owners and team members

---
+++ service-team/src/main/java/sg/ncl/service/team/domain/TeamMemberType.java
@@ -0,0 +1,10 @@
+package sg.ncl.service.team.domain;
+
+/**
+ * The {@link TeamMemberType} enumerates all the types of membership of a {@link Team}.
+ *
+ * @author Te Ye
+ */
+public enum TeamMemberType {
+    OWNER, MEMBER
+}
