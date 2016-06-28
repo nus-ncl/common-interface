25a7145fc25596e222585e5310fb04f631ca02e7
Christopher Zhong <ChrisZhong@users.noreply.github.com>
2016-06-28 14:23:46 +0800

    DEV-387 Refactor `JpaConfig` to `JpaAutoConfiguration`

--- common-jpa/src/main/java/sg/ncl/common/jpa/JpaAutoConfiguration.java
+++ common-jpa/src/main/java/sg/ncl/common/jpa/JpaAutoConfiguration.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -17,10 +18,11 @@ import javax.sql.DataSource;
  */
 @Configuration
 @ConditionalOnClass(DataSource.class)
+@EnableConfigurationProperties(DataSourceProperties.class)
 @PropertySource("classpath:/sg/ncl/common/jpa/jpa.properties")
-public class JpaConfig {
+public class JpaAutoConfiguration {
 
-    private static final Logger logger = LoggerFactory.getLogger(JpaConfig.class);
+    private static final Logger logger = LoggerFactory.getLogger(JpaAutoConfiguration.class);
 
     @Inject
     private DataSourceProperties dataSourceProperties;
