99dfd795eae0b78d5d3ba6bc541df1e3d3d1c178
Christopher Zhong <ChrisZhong@users.noreply.github.com>
2016-06-28 15:01:40 +0800

    DEV-387 Remove `@WebAppConfiguration`

--- adapter-deterlab/src/test-integration/java/sg/ncl/adapter/deterlab/AbstractTest.java
+++ adapter-deterlab/src/test-integration/java/sg/ncl/adapter/deterlab/AbstractTest.java
@@ -1,10 +1,9 @@
 package sg.ncl.adapter.deterlab;
 
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import sg.ncl.adapter.deterlab.data.jpa.App;
 
 /**
  * @author Christopher Zhong
  */
-@SpringApplicationConfiguration(App.class)
+@SpringApplicationConfiguration(TestApp.class)
 public abstract class AbstractTest extends sg.ncl.common.test.AbstractTest {}
--- adapter-deterlab/src/test-integration/java/sg/ncl/adapter/deterlab/TestApp.java
+++ adapter-deterlab/src/test-integration/java/sg/ncl/adapter/deterlab/TestApp.java
@@ -1,20 +1,20 @@
-package sg.ncl.adapter.deterlab.data.jpa;
+package sg.ncl.adapter.deterlab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
-import sg.ncl.adapter.deterlab.DeterlabAutoConfiguration;
 import sg.ncl.common.jpa.UseJpa;
 
 /**
  * @author Christopher Zhong
  */
 @SpringBootApplication
+@UseJpa
 @Import(DeterlabAutoConfiguration.class)
-public class App {
+public class TestApp {
 
     public static void main(final String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(TestApp.class, args);
     }
 
 }
