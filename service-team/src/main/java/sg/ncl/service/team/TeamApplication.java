9b8f87265249cd225ff1e1828a1bf3c269ef8b34
teye <a0097953@gmail.com>
2016-06-29 14:05:02 +0800

    refactor App to TeamApplication

--- service-team/src/main/java/sg/ncl/service/team/TeamApplication.java
+++ service-team/src/main/java/sg/ncl/service/team/TeamApplication.java
@@ -9,10 +9,10 @@ import sg.ncl.common.jpa.UseJpa;
  */
 @SpringBootApplication
 @UseJpa
-public class App {
+public class TeamApplication {
 
     public static void main(final String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(TeamApplication.class, args);
     }
 
 }
