8e5293774cc649e2118b1d8d8790e34c160ca919
Christopher Zhong <ChrisZhong@users.noreply.github.com>
2016-06-29 13:22:34 +0800

    DEV-387 Replace `@UseJwt` with `@Import`

--- common-jwt/src/test-integration/java/sg/ncl/common/jwt/TestApp.java
+++ common-jwt/src/test-integration/java/sg/ncl/common/jwt/TestApp.java
@@ -2,13 +2,14 @@ package sg.ncl.common.jwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 /**
  * @author Christopher Zhong
  * @version 1.0
  */
 @SpringBootApplication
-@UseJwt
+@Import(JwtAutoConfiguration.class)
 public class TestApp {
     public static void main(String[] args) {
         SpringApplication.run(TestApp.class, args);
