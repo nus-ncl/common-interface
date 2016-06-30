167621a06c9c94d0c74118d3e8ebe48f9d434d18
Christopher Zhong <ChrisZhong@users.noreply.github.com>
2016-06-30 12:01:13 +0800

    DEV-387 Remove unnecessary constructors

--- service-authentication/src/main/java/sg/ncl/service/authentication/exceptions/NeitherUsernameNorPasswordModifiedException.java
+++ service-authentication/src/main/java/sg/ncl/service/authentication/exceptions/NeitherUsernameNorPasswordModifiedException.java
@@ -7,22 +7,8 @@ import sg.ncl.common.exception.NotModifiedException;
  */
 public class NeitherUsernameNorPasswordModifiedException extends NotModifiedException {
 
-    public NeitherUsernameNorPasswordModifiedException() {}
-
-    public NeitherUsernameNorPasswordModifiedException(final String message) {
-        super(message);
-    }
-
-    public NeitherUsernameNorPasswordModifiedException(final String message, final Throwable cause) {
-        super(message, cause);
-    }
-
-    public NeitherUsernameNorPasswordModifiedException(final Throwable cause) {
-        super(cause);
-    }
-
-    protected NeitherUsernameNorPasswordModifiedException(final String message, final Throwable cause, final boolean enableSuppression, final boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
+    public NeitherUsernameNorPasswordModifiedException() {
+        super("Username and/or password is unchanged");
     }
 
 }
--- service-authentication/src/main/java/sg/ncl/service/authentication/exceptions/UserIdNullOrEmptyException.java
+++ service-authentication/src/main/java/sg/ncl/service/authentication/exceptions/UserIdNullOrEmptyException.java
@@ -7,22 +7,8 @@ import sg.ncl.common.exception.BadRequestException;
  */
 public class UserIdNullOrEmptyException extends BadRequestException {
 
-    public UserIdNullOrEmptyException() {}
-
-    public UserIdNullOrEmptyException(final String message) {
-        super(message);
-    }
-
-    public UserIdNullOrEmptyException(final String message, final Throwable cause) {
-        super(message, cause);
-    }
-
-    public UserIdNullOrEmptyException(final Throwable cause) {
-        super(cause);
-    }
-
-    protected UserIdNullOrEmptyException(final String message, final Throwable cause, final boolean enableSuppression, final boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
+    public UserIdNullOrEmptyException() {
+        super("User ID cannot be null or empty");
     }
 
 }
