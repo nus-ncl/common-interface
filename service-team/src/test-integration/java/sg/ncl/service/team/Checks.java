f19b7ee5f35cae04ad5c2d3c66ec3ed7c842435a
teye <a0097953@gmail.com>
2016-07-12 15:42:21 +0800

    fix team tests

---
+++ service-team/src/test-integration/java/sg/ncl/service/team/Checks.java
@@ -0,0 +1,20 @@
+package sg.ncl.service.team;
+
+import org.springframework.web.util.NestedServletException;
+import sg.ncl.service.team.exceptions.TeamNotFoundException;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Te Ye
+ */
+public class Checks {
+
+    public static void checkException(final Exception e, final String message) {
+        assertThat(e, is(instanceOf(NestedServletException.class)));
+        assertThat(e.getCause(), is(instanceOf(TeamNotFoundException.class)));
+        assertThat(e.getCause().getMessage(), containsString(message));
+    }
+
+}
