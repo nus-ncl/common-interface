ea39d41a719c85d377789ee2c48e297507fb5167
Christopher Zhong <chriszhong@users.noreply.github.com>
2016-08-06 17:11:22 +0800

    DEV-389 apply Lombok to Jwt classes

--- common/src/main/java/sg/ncl/common/jwt/JwtAutoConfiguration.java
+++ common/src/main/java/sg/ncl/common/jwt/JwtAutoConfiguration.java
@@ -2,8 +2,7 @@ package sg.ncl.common.jwt;
 
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.SignatureException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -21,19 +20,20 @@ import java.time.format.DateTimeParseException;
 import java.util.UUID;
 
 /**
+ * Initializes the necessary components to create JWTs.
+ *
  * @author Christopher Zhong
  * @version 1.0
  */
 @Configuration
 @ConditionalOnClass({BCryptPasswordEncoder.class, SignatureAlgorithm.class, Key.class, Duration.class})
 @EnableConfigurationProperties(JwtProperties.class)
+@Slf4j
 public class JwtAutoConfiguration {
 
-    private static final Logger logger = LoggerFactory.getLogger(JwtAutoConfiguration.class);
-
     private static final SignatureAlgorithm DEFAULT_SIGNATURE_ALGORITHM = SignatureAlgorithm.HS512;
-    private static final Duration DEFAULT_EXPIRY_DURATION = Duration.ofHours(24L);
     private static final UUID DEFAULT_API_KEY = UUID.randomUUID();
+    private static final Duration DEFAULT_EXPIRY_DURATION = Duration.ofHours(24L);
 
     private final JwtProperties properties;
 
@@ -53,28 +53,28 @@ public class JwtAutoConfiguration {
     public SignatureAlgorithm signatureAlgorithm() {
         final String value = properties.getSigningAlgorithm();
         if (value == null || value.isEmpty()) {
-            logger.warn("No signature algorithm was specified; using default: {}", DEFAULT_SIGNATURE_ALGORITHM);
+            log.warn("No signature algorithm was specified; using default: {}", DEFAULT_SIGNATURE_ALGORITHM);
             return DEFAULT_SIGNATURE_ALGORITHM;
         }
         try {
             final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.forName(value);
-            logger.info("Using specified signature algorithm: {}", signatureAlgorithm);
+            log.info("Using specified signature algorithm: {}", signatureAlgorithm);
             return signatureAlgorithm;
         } catch (SignatureException e) {
-            logger.warn("Signature algorithm '{}' is unknown; using default: {}", value, DEFAULT_SIGNATURE_ALGORITHM);
+            log.warn("SignatureException: '{}'; signature algorithm '{}' is unknown; using default: {}", e, value, DEFAULT_SIGNATURE_ALGORITHM);
             return DEFAULT_SIGNATURE_ALGORITHM;
         }
     }
 
     @Bean
     @ConditionalOnMissingBean(Key.class)
-    public Key apiKey(final SignatureAlgorithm signatureAlgorithm) {
+    public Key apiKey(@NotNull final SignatureAlgorithm signatureAlgorithm) {
         final String value = properties.getApiKey();
         if (value == null || value.isEmpty()) {
-            logger.warn("No api key was specified; using default: {}", DEFAULT_API_KEY);
+            log.warn("No api key was specified; using default: {}", DEFAULT_API_KEY);
             return new SecretKeySpec(DEFAULT_API_KEY.toString().getBytes(), signatureAlgorithm.getJcaName());
         }
-        logger.info("Using specified api key: {}", value);
+        log.info("Using specified api key: {}", value);
         return new SecretKeySpec(value.getBytes(), signatureAlgorithm.getJcaName());
     }
 
@@ -83,15 +83,15 @@ public class JwtAutoConfiguration {
     public Duration expiryDuration() {
         final String value = properties.getExpiryDuration();
         if (value == null || value.isEmpty()) {
-            logger.warn("No expiry duration was specified; using default: {}", DEFAULT_EXPIRY_DURATION);
+            log.warn("No expiry duration was specified; using default: {}", DEFAULT_EXPIRY_DURATION);
             return DEFAULT_EXPIRY_DURATION;
         }
         try {
             final Duration duration = Duration.parse(value);
-            logger.info("Using specified expiry duration: {}", duration);
+            log.info("Using specified expiry duration: {}", duration);
             return duration;
         } catch (DateTimeParseException e) {
-            logger.warn("Duration format '{}' is invalid; using default: {}", value, DEFAULT_EXPIRY_DURATION);
+            log.warn("DateTimeParseException: '{}'; duration format '{}' is invalid; using default: {}", e, value, DEFAULT_EXPIRY_DURATION);
             return DEFAULT_EXPIRY_DURATION;
         }
     }
