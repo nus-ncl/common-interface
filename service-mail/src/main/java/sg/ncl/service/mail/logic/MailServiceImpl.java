3d427630b3ca53e0ce3749453f222bc8b0e5949d
Zhang Chunwang <zhangchunwang@gmail.com>
2016-08-10 15:29:09 +0800

    Created a new micro service service-mail

--- service-mail/src/main/java/sg/ncl/service/mail/logic/MailServiceImpl.java
+++ service-mail/src/main/java/sg/ncl/service/mail/logic/MailServiceImpl.java
@@ -1,39 +1,48 @@
 package sg.ncl.service.mail.logic;
 
-import java.util.Properties;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import sg.ncl.service.mail.domain.MailService;
+
+import javax.inject.Inject;
+import javax.validation.constraints.NotNull;
+
 
 /**
  * Created by dcszwang on 8/10/2016.
  */
-public class MailServiceImpl {
 
-    public void sendMail() throws MessagingException {
-        JavaMailSenderImpl sender = new JavaMailSenderImpl();
-        Properties props = new Properties();
-        props.put("mail.smtp.host", "smtp.gmail.com"); //SMTP Host
-        props.put("mail.smtp.socketFactory.port", "465"); //SSL Port
-        props.put("mail.smtp.socketFactory.class",
-                "javax.net.ssl.SSLSocketFactory"); //SSL Factory Class
-        props.put("mail.smtp.auth", "true"); //Enabling SMTP Authentication
-        props.put("mail.smtp.port", "465"); //SMTP Port
-        //props.put("mail.smtp.starttls.enable", "true");
+@Service
+@Slf4j
+public class MailServiceImpl implements MailService{
 
-        //sender.setHost("smtp.gmail.com");
-        //sender.setPort(587);
-        sender.setUsername("testbed-ops@ncl.sg");
-        //sender.setUsername("zhangchunwang@gmail.com");
-        sender.setPassword("deterinavm");
+    JavaMailSender sender;
 
-        sender.setJavaMailProperties(props);
-
-        MimeMessage message = sender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message);
-        helper.setTo("dcszwang@nus.edu.sg");
-        helper.setText("Testing email from NCL testbed.");
-        helper.setSubject("Test Email");
-        //helper.setFrom("testbed-ops@ncl.sg");
-
-
-        sender.send(message);
+    @Inject
+    MailServiceImpl(@NotNull final JavaMailSender sender) {
+        this.sender = sender;
     }
+
+    public void send(@NotNull final String from, @NotNull final String to, @NotNull final String subject, @NotNull final String content) {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setFrom(from);
+        msg.setTo(to);
+        msg.setSubject(subject);
+        msg.setText(content);
+
+        try {
+            sender.send(msg);
+            log.info("Email sent. From: '{}', To: '{}', Subject: '{}'", from, to, subject);
+        }
+        catch (MailException ex) {
+            log.warn("Sending email failed. From: '{}', To: '{}', Subject: '{}'",
+                    from, to, subject);
+            throw ex;
+        }
+    }
+
+
 }
