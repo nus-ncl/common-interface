bd21a55e144e19b4f8f68840ba4f379455b9be7c
Zhang Chunwang <zhangchunwang@gmail.com>
2016-09-07 14:12:29 +0800

    DEV-505 Added unit tests for mail service

--- service-mail/src/test/java/sg/ncl/service/mail/data/jpa/EmailEntityTest.java
+++ service-mail/src/test/java/sg/ncl/service/mail/data/jpa/EmailEntityTest.java
@@ -6,9 +6,10 @@ import org.junit.Test;
 import java.time.ZonedDateTime;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.nullValue;
 
 /**
@@ -172,5 +173,62 @@ public class EmailEntityTest {
         final String error = "error message";
         emailEntity.setErrorMessage(error);
         assertThat(emailEntity.getErrorMessage(), is(equalTo(error)));
+    }
+
+    @Test
+    public void testEquals() {
+        final EmailEntity entity1 = new EmailEntity();
+        final EmailEntity entity2 = new EmailEntity();
+        assertThat(entity1, is(equalTo(entity2)));
+
+        final Long id1 = Long.parseLong(RandomStringUtils.randomNumeric(10));
+        entity1.setId(id1);
+        assertThat(entity1, is(not(equalTo(entity2))));
+
+        entity2.setId(id1);
+        assertThat(entity1, is(equalTo(entity2)));
+
+        final Long id2 = Long.parseLong(RandomStringUtils.randomNumeric(10));
+        entity2.setId(id2);
+        assertThat(entity1, is(not(equalTo(entity2))));
+    }
+
+    @Test
+    public void testHashCode() {
+        final EmailEntity entity1 = new EmailEntity();
+        final EmailEntity entity2 = new EmailEntity();
+        assertThat(entity1.hashCode(), is(equalTo(entity2.hashCode())));
+
+        final Long id1 = Long.parseLong(RandomStringUtils.randomNumeric(10));
+        entity1.setId(id1);
+        assertThat(entity1.hashCode(), is(not(equalTo(entity2.hashCode()))));
+
+        entity2.setId(id1);
+        assertThat(entity1.hashCode(), is(equalTo(entity2.hashCode())));
+
+        final Long id2 = Long.parseLong(RandomStringUtils.randomNumeric(10));
+        entity2.setId(id2);
+        assertThat(entity1.hashCode(), is(not(equalTo(entity2.hashCode()))));
+    }
+
+    @Test
+    public void testToString() {
+        final EmailEntity entity = new EmailEntity();
+        final Long id = Long.parseLong(RandomStringUtils.randomNumeric(10));
+        entity.setId(id);
+        entity.setSender("alice@ncl.sg");
+        entity.setRecipients(new String[] {"bob@ncl.sg"});
+        entity.setSubject("subject");
+        entity.setContent("content");
+        entity.setHtml(false);
+        final String toString = entity.toString();
+        String[] to = entity.getRecipients();
+
+        assertThat(toString, containsString(entity.getId().toString()));
+        assertThat(toString, containsString(entity.getSender()));
+        assertThat(toString, containsString(to[0]));
+        assertThat(toString, containsString(entity.getSubject()));
+        assertThat(toString, containsString(entity.getContent()));
+        assertThat(toString, containsString(String.valueOf(false)));
     }
 }
