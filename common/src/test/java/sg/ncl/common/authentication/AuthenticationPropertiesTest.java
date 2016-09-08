2e0d1cd134d562910080fd6cddcc03679df42ad6
Christopher Zhong <chriszhong@users.noreply.github.com>
2016-09-08 19:05:49 +0800

    DEV-14 update `AuthenticationPropertiesTest` to be more efficient

--- common/src/test/java/sg/ncl/common/authentication/AuthenticationPropertiesTest.java
+++ common/src/test/java/sg/ncl/common/authentication/AuthenticationPropertiesTest.java
@@ -2,28 +2,35 @@ package sg.ncl.common.authentication;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.junit4.SpringRunner;
+import sg.ncl.common.authentication.AuthenticationPropertiesTest.TestConfig;
 
 import javax.inject.Inject;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created by Chris on 8/6/2016.
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = TestSpringBootApp.class)
+@SpringBootTest(classes = TestConfig.class, webEnvironment = WebEnvironment.NONE)
 public class AuthenticationPropertiesTest {
+
+    @Configuration
+    @EnableConfigurationProperties(AuthenticationProperties.class)
+    static class TestConfig {
+    }
 
     @Inject
     private AuthenticationProperties properties;
 
     @Test
     public void testGetUrl() throws Exception {
-        assertThat(properties.getUrl(), is(equalTo("/authentications")));
+        assertThat(properties.getUrl()).isEqualTo("/testAuthenticationsUrl");
     }
 
 }
