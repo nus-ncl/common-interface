0a8598ed07b0f523735758fb18efe073203ef6c4
Christopher Zhong <chriszhong@users.noreply.github.com>
2016-09-08 19:08:51 +0800

    DEV-14 update test to use AssertJ syntax

--- common/src/test/java/sg/ncl/common/authentication/AuthenticationAutoConfigurationTest.java
+++ common/src/test/java/sg/ncl/common/authentication/AuthenticationAutoConfigurationTest.java
@@ -9,11 +9,7 @@ import org.mockito.junit.MockitoRule;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.not;
-import static org.hamcrest.Matchers.nullValue;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created by Chris on 8/6/2016.
@@ -36,8 +32,8 @@ public class AuthenticationAutoConfigurationTest {
     public void testPasswordEncoder() throws Exception {
         final PasswordEncoder encoder = configuration.passwordEncoder();
 
-        assertThat(encoder, is(not(nullValue(PasswordEncoder.class))));
-        assertThat(encoder, is(instanceOf(BCryptPasswordEncoder.class)));
+        assertThat(encoder).isNotNull();
+        assertThat(encoder).isInstanceOf(BCryptPasswordEncoder.class);
     }
 
 }
