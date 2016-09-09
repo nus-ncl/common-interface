078e9e70705495f16a4a45286f466073055f21ed
teye <a0097953@gmail.com>
2016-09-09 11:49:55 +0800

    DEV-14 change import

--- common/src/test/java/sg/ncl/common/authentication/TestSpringBootApp.java
+++ common/src/test/java/sg/ncl/common/authentication/TestSpringBootApp.java
@@ -3,13 +3,14 @@ package sg.ncl.common.authentication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import sg.ncl.common.jwt.JwtFilter;
 
 /**
  * @author Christopher Zhong
  * @version 1.0
  */
 @SpringBootApplication
-@Import(AuthenticationAutoConfiguration.class)
+@Import({AuthenticationAutoConfiguration.class, JwtFilter.class})
 public class TestSpringBootApp {
 
     public static void main(String[] args) {
