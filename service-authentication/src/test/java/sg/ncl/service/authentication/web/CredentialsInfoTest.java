20f8b9d12fcb651646dcf8be64166a4a7ad5eedb
Christopher Zhong <chriszhong@users.noreply.github.com>
2016-09-15 23:44:14 +0800

    DEV-504 update to AssertJ syntax and add missing test

--- service-authentication/src/test/java/sg/ncl/service/authentication/web/CredentialsInfoTest.java
+++ service-authentication/src/test/java/sg/ncl/service/authentication/web/CredentialsInfoTest.java
@@ -2,45 +2,47 @@ package sg.ncl.service.authentication.web;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Test;
-import sg.ncl.service.authentication.domain.CredentialsStatus;
 import sg.ncl.common.authentication.Role;
+import sg.ncl.service.authentication.domain.CredentialsStatus;
 
 import java.util.Arrays;
-import java.util.HashSet;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author Christopher Zhong
  */
 public class CredentialsInfoTest {
 
+    private final String id = RandomStringUtils.randomAlphanumeric(20);
     private final String username = RandomStringUtils.randomAlphanumeric(20);
     private final String password = RandomStringUtils.randomAlphanumeric(20);
-    private final String userId = RandomStringUtils.randomAlphanumeric(20);
-    private final CredentialsInfo credentialsInfo = new CredentialsInfo(userId, username, password, CredentialsStatus.ACTIVE, new HashSet<>(Arrays.asList(Role.ADMIN, Role.USER)));
+    private final CredentialsInfo credentialsInfo = new CredentialsInfo(id, username, password, CredentialsStatus.ACTIVE, Arrays.asList(Role.ADMIN, Role.ADMIN, Role.USER));
+
+    @Test
+    public void testGetId() throws Exception {
+        assertThat(credentialsInfo.getId()).isEqualTo(id);
+    }
 
     @Test
     public void testGetUsername() throws Exception {
-        assertThat(credentialsInfo.getUsername(), is(equalTo(username)));
+        assertThat(credentialsInfo.getUsername()).isEqualTo(username);
     }
 
     @Test
     public void testGetPassword() throws Exception {
-        assertThat(credentialsInfo.getPassword(), is(equalTo(password)));
+        assertThat(credentialsInfo.getPassword()).isEqualTo(password);
 
-    }
-
-    @Test
-    public void testGetUserId() throws Exception {
-        assertThat(credentialsInfo.getId(), is(equalTo(userId)));
     }
 
     @Test
     public void testGetStatus() throws Exception {
-        assertThat(credentialsInfo.getStatus(), is(equalTo(CredentialsStatus.ACTIVE)));
+        assertThat(credentialsInfo.getStatus()).isEqualTo(CredentialsStatus.ACTIVE);
+    }
+
+    @Test
+    public void testGetRoles() throws Exception {
+        assertThat(credentialsInfo.getRoles()).containsOnly(Role.ADMIN, Role.USER).hasSize(2);
     }
 
 }
