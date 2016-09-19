40e227af646ffe6fd80c751b00a62820fbae95cf
Christopher Zhong <ChrisZhong@users.noreply.github.com>
2016-09-19 16:22:14 +0800

    DEV-504 optimize test case

--- service-authentication/src/test/java/sg/ncl/service/authentication/web/AuthenticationControllerTest.java
+++ service-authentication/src/test/java/sg/ncl/service/authentication/web/AuthenticationControllerTest.java
@@ -2,27 +2,28 @@ package sg.ncl.service.authentication.web;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.util.Base64Utils;
-import org.springframework.web.context.WebApplicationContext;
-import sg.ncl.service.authentication.AbstractTest;
-import sg.ncl.service.authentication.domain.AuthenticationService;
 import sg.ncl.common.authentication.Role;
+import sg.ncl.common.exception.ExceptionAutoConfiguration;
+import sg.ncl.common.exception.GlobalExceptionHandler;
+import sg.ncl.service.authentication.domain.AuthenticationService;
 
 import javax.inject.Inject;
-
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mockingDetails;
 import static org.mockito.Mockito.when;
@@ -34,63 +35,65 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 /**
  * @author Christopher Zhong
  */
-@ActiveProfiles("mock-authentication-service")
-public class AuthenticationControllerTest extends AbstractTest {
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = AuthenticationController.class, secure = false)
+@ContextConfiguration(classes = {AuthenticationController.class, ExceptionAutoConfiguration.class, GlobalExceptionHandler.class})
+public class AuthenticationControllerTest {
 
-    private final MockHttpServletRequestBuilder post = post("/authentication");
-
+    public static final String AUTHENTICATION = "/authentication";
     @Inject
-    private WebApplicationContext webApplicationContext;
-    @Inject
-    private AuthenticationService authenticationService;
-
     private MockMvc mockMvc;
+    @MockBean
+    private AuthenticationService authenticationService;
 
     @Before
     public void before() {
-        assertThat(mockingDetails(authenticationService).isMock(), is(true));
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        assertThat(mockingDetails(authenticationService).isMock()).isTrue();
     }
 
     @Test
     public void testLoginGood() throws Exception {
-        final Set<Role> roles = new HashSet<>(Arrays.asList(Role.USER));
+        final List<Role> roles = Arrays.asList(Role.USER);
         final String username = "username";
         final String password = "password";
         final String s = Base64Utils.encodeToString((username + ":" + password).getBytes());
 
         when(authenticationService.login(eq(username), eq(password))).thenReturn(new AuthorizationInfo("id", "jwt token", roles));
 
-        mockMvc.perform(post.header(HttpHeaders.AUTHORIZATION, "Basic " + s))
+        mockMvc.perform(post(AUTHENTICATION).header(HttpHeaders.AUTHORIZATION, "Basic " + s))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.id", is(equalTo("id"))))
                 .andExpect(jsonPath("$.token", is(equalTo("jwt token"))))
+                .andExpect(jsonPath("$.roles", hasSize(1)))
                 .andExpect(jsonPath("$.roles[0]", is(equalTo("USER"))));
     }
 
     @Test
     public void testLoginMissingAuthorization() throws Exception {
-        mockMvc.perform(post)
+        mockMvc.perform(post(AUTHENTICATION))
                 .andExpect(status().isBadRequest());
     }
 
     @Test
     public void testLoginEmptyAuthorization() throws Exception {
-        mockMvc.perform(post.header(HttpHeaders.AUTHORIZATION, ""))
-                .andExpect(status().isBadRequest());
+        mockMvc.perform(post(AUTHENTICATION).header(HttpHeaders.AUTHORIZATION, ""))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
     }
 
     @Test
     public void testLoginInvalidBasicAuthentication() throws Exception {
-        mockMvc.perform(post.header(HttpHeaders.AUTHORIZATION, "Basic authentication"))
-                .andExpect(status().isBadRequest());
+        mockMvc.perform(post(AUTHENTICATION).header(HttpHeaders.AUTHORIZATION, "Basic authentication"))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
     }
 
     @Test
     public void testLoginUnknownAuthorizationScheme() throws Exception {
-        mockMvc.perform(post.header(HttpHeaders.AUTHORIZATION, "Unknown scheme"))
-                .andExpect(status().isBadRequest());
+        mockMvc.perform(post(AUTHENTICATION).header(HttpHeaders.AUTHORIZATION, "Unknown scheme"))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
     }
 
 }
