65e2142886867c33fb0c306769f9eba5a1423dc2
teye <a0097953@gmail.com>
2016-09-19 15:40:32 +0800

    DEV-603 add more endpoints to authautoconfig

--- common/src/main/java/sg/ncl/common/jwt/JwtAutoConfiguration.java
+++ common/src/main/java/sg/ncl/common/jwt/JwtAutoConfiguration.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 
 import javax.crypto.spec.SecretKeySpec;
 import javax.inject.Inject;
--- common/src/main/java/sg/ncl/common/jwt/JwtToken.java
+++ common/src/main/java/sg/ncl/common/jwt/JwtToken.java
@@ -7,10 +7,7 @@ import sg.ncl.common.authentication.Role;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 /**
  * @author Te Ye
