d83f36e4f5577c4971a09398a0d7ef76e6886faa
teye <a0097953@gmail.com>
2016-09-20 16:30:16 +0800

    DEV-603 move import classes for tests

--- common/src/test/java/sg/ncl/common/authentication/TestConfig.java
+++ common/src/test/java/sg/ncl/common/authentication/TestConfig.java
@@ -3,7 +3,11 @@ package sg.ncl.common.authentication;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import sg.ncl.common.jwt.JwtAuthenticationProvider;
+import sg.ncl.common.jwt.JwtAutoConfiguration;
+import sg.ncl.common.jwt.JwtFilter;
 
 import static org.mockito.Mockito.mock;
 
@@ -11,6 +15,7 @@ import static org.mockito.Mockito.mock;
  * @author Christopher Zhong
  */
 @Configuration
+@Import({JwtFilter.class, JwtAuthenticationProvider.class, JwtAutoConfiguration.class})
 @Slf4j
 public class TestConfig {
     @Bean
