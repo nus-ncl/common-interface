e9c1ecc3c2f60b1c71a255e5c90f5774ce674f51
teye <a0097953@gmail.com>
2016-09-20 16:22:19 +0800

    DEV-603 update test codes

---
+++ common/src/test/java/sg/ncl/common/authentication/AbstractTest.java
@@ -0,0 +1,15 @@
+package sg.ncl.common.authentication;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.web.WebAppConfiguration;
+import sg.ncl.common.jwt.JwtAuthenticationProvider;
+import sg.ncl.common.jwt.JwtFilter;
+
+/**
+ * @author Christopher Zhong
+ */
+@SpringBootTest(classes = AuthenticationAutoConfiguration.class)
+@WebAppConfiguration
+@Import({JwtFilter.class, JwtAuthenticationProvider.class})
+public abstract class AbstractTest extends sg.ncl.common.test.AbstractTest {}
