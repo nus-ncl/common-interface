da7d38bf6a02610c891560bfb12dff57c841f1d2
teye <a0097953@gmail.com>
2016-09-20 16:53:12 +0800

    DEV-603 fix team controller tests

--- service-team/src/test/java/sg/ncl/service/team/web/TeamsControllerTest.java
+++ service-team/src/test/java/sg/ncl/service/team/web/TeamsControllerTest.java
@@ -10,6 +10,9 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -32,6 +35,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -60,6 +65,11 @@ public class TeamsControllerTest extends AbstractTest {
     @Before
     public void setup() {
         mockMvc = webAppContextSetup(webApplicationContext).build();
+        // need this because controller checks for authentication for some endpoints
+        Authentication authentication = mock(Authentication.class);
+        SecurityContext securityContext = mock(SecurityContext.class);
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        SecurityContextHolder.setContext(securityContext);
     }
 
     @Test
