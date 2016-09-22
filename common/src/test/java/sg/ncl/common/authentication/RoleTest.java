b39f78963e652c4648babb6c1b61a842af051b76
Christopher Zhong <chriszhong@users.noreply.github.com>
2016-09-22 19:58:36 +0800

    DEV-603 add test for role

--- common/src/test/java/sg/ncl/common/authentication/RoleTest.java
+++ common/src/test/java/sg/ncl/common/authentication/RoleTest.java
@@ -2,7 +2,7 @@ package sg.ncl.common.authentication;
 
 import org.junit.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author Te Ye
@@ -10,9 +10,15 @@ import static org.assertj.core.api.Assertions.*;
 public class RoleTest {
 
     @Test
-    public void testRole() throws Exception {
+    public void testRoleAdmin() throws Exception {
         final Role role = Role.ADMIN;
-        assertThat(role.getAuthority()).isEqualTo(role.toString());
+        assertThat(role.getAuthority()).isEqualTo("ADMIN");
+    }
+
+    @Test
+    public void testRoleUser() throws Exception {
+        final Role role = Role.USER;
+        assertThat(role.getAuthority()).isEqualTo("USER");
     }
 
 }
