0eb6f0aac779119299b9ffac2f1bf89085cbffbf
teye <a0097953@gmail.com>
2016-09-30 16:18:20 +0800

    DEV-634 refactor validator

--- service-experiment/src/main/java/sg/ncl/service/experiment/validation/Validator.java
+++ service-experiment/src/main/java/sg/ncl/service/experiment/validation/Validator.java
@@ -17,27 +17,33 @@ import java.util.ArrayList;
 public class Validator {
 
     public static void addCheck(final RealizationEntity realizationEntity, final Claims claims) {
+        checkClaimsRolesType(realizationEntity, claims);
         checkPermissions(realizationEntity, claims);
     }
 
-    public static void checkPermissions(RealizationEntity realizationEntity, Claims claims) {
+    public static void checkClaimsRolesType(final RealizationEntity realizationEntity, final Claims claims) {
+        if (!(claims.get(JwtToken.KEY) instanceof ArrayList<?>)) {
+            log.warn("Bad claims type found: {}", claims);
+            throw new ForbiddenException("Invalid permissions for delete experiment: expid " + realizationEntity.getExperimentId());
+        }
+    }
+
+    public static void checkPermissions(final RealizationEntity realizationEntity, final Claims claims) {
+        // since need to pass the checkClaimsRolesType
+        // safe to say the role is of type Arraylist at this stage
         log.info("Id of requester from web: {}", claims.getSubject());
         log.info("Role of requester from web: {}", claims.get(JwtToken.KEY));
         String contextUserId = claims.getSubject();
         ArrayList<Role> roles;
+        roles = (ArrayList) claims.get(JwtToken.KEY);
 
-        if (claims.get(JwtToken.KEY) instanceof ArrayList<?>) {
-            roles = (ArrayList) claims.get(JwtToken.KEY);
-            log.info("Context user id: {}, Context role: {}", contextUserId, roles);
+        log.info("Context user id: {}, Context role: {}", contextUserId, roles);
 
-            if (!contextUserId.equals(realizationEntity.getUserId()) && !roles.contains(Role.ADMIN)) {
-                log.warn("Access denied for delete experiment: /{}/ ", realizationEntity.getExperimentId());
-                throw new ForbiddenException("Access denied for delete experiment: expid " + realizationEntity.getExperimentId());
-            }
-        } else {
-            log.warn("Bad claims type found: {}", claims);
-            throw new ForbiddenException("Invalid permissions for delete experiment: expid " + realizationEntity.getExperimentId());
+        if (!contextUserId.equals(realizationEntity.getUserId()) && !roles.contains(Role.ADMIN)) {
+            log.warn("Access denied for delete experiment: /{}/ ", realizationEntity.getExperimentId());
+            throw new ForbiddenException("Access denied for delete experiment: expid " + realizationEntity.getExperimentId());
         }
+
     }
 
 }
