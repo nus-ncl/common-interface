eadb5c257b4f365d88c363f99d59bfcf4f0ded81
Tran Ly Vu <vutransingapore@gmail.com>
2016-10-03 17:47:01 +0800

    Update test for DeterLabProjectEntity in adapter-deterlab

--- adapter-deterlab/src/test/java/sg/ncl/adapter/deterlab/dtos/entities/DeterLabProjectEntityTest.java
+++ adapter-deterlab/src/test/java/sg/ncl/adapter/deterlab/dtos/entities/DeterLabProjectEntityTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created by Tran Ly Vu on 10/3/2016.
@@ -26,6 +27,10 @@ public class DeterLabProjectEntityTest {
         String actual1=deterLabUserEntity.getId();
         String actual2=deterLabUserEntity.getNclTeamId();
         String actual3=deterLabUserEntity.getDeterProjectId();
+
+        assertThat(actual1).isNotNull();
+        assertThat(actual2).isNotNull();
+        assertThat(actual3).isNotNull();
 
         assertEquals("test1",actual1);
         assertEquals("test2",actual2);
