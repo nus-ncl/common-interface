7e2a9f6d287cc302a90ce8de482d292924d516db
Tran Ly Vu <dcstlvu@nus.edu.sg>
2016-10-03 16:25:41 +0800

    Update unit testing for class DeterlabProjectEntity in adapter-deterlab (SIO)

---
+++ adapter-deterlab/src/test/java/sg/ncl/adapter/deterlab/dtos/entities/DeterLabProjectEntityTest.java
@@ -0,0 +1,35 @@
+package sg.ncl.adapter.deterlab.dtos.entities;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Tran Ly Vu on 10/3/2016.
+ */
+
+public class DeterLabProjectEntityTest {
+    private DeterLabProjectEntity deterLabUserEntity;
+
+    @Before
+    public void setup(){
+        deterLabUserEntity=new DeterLabProjectEntity();
+    }
+
+    @Test
+    public void getterAndSetterTest(){
+        deterLabUserEntity.setId("test1");
+        deterLabUserEntity.setNclTeamId("test2");
+        deterLabUserEntity.setDeterProjectId("test3");
+
+        String actual1=deterLabUserEntity.getId();
+        String actual2=deterLabUserEntity.getNclTeamId();
+        String actual3=deterLabUserEntity.getDeterProjectId();
+
+        assertEquals("test1",actual1);
+        assertEquals("test2",actual2);
+        assertEquals("test3",actual3);
+    }
+
+}
