4be483fb321af4a6e271239f5ddc3aef0d671fb3
teye <a0097953@gmail.com>
2016-10-05 10:40:46 +0800

    DEV-661 fix credential controller tests

--- service-authentication/src/test/java/sg/ncl/service/authentication/web/CredentialsControllerTest.java
+++ service-authentication/src/test/java/sg/ncl/service/authentication/web/CredentialsControllerTest.java
@@ -5,12 +5,18 @@ import io.jsonwebtoken.Claims;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import sg.ncl.common.exception.ExceptionAutoConfiguration;
 import sg.ncl.common.exception.GlobalExceptionHandler;
 import sg.ncl.service.authentication.data.jpa.CredentialsEntity;
@@ -45,7 +51,7 @@ import static sg.ncl.service.authentication.util.TestUtil.getCredentialsEntity;
  * @author Christopher Zhong
  */
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = CredentialsController.class, secure = false)
+@WebMvcTest(controllers = CredentialsController.class, secure = true)
 @ContextConfiguration(classes = {CredentialsController.class, ExceptionAutoConfiguration.class, GlobalExceptionHandler.class})
 public class CredentialsControllerTest {
 
@@ -53,12 +59,30 @@ public class CredentialsControllerTest {
     private ObjectMapper mapper;
     @Inject
     private MockMvc mockMvc;
+    @Inject
+    private WebApplicationContext webApplicationContext;
+
+    @Mock
+    private Claims claims;
+    @Mock
+    private Authentication authentication;
+    @Mock
+    private SecurityContext securityContext;
+
     @MockBean
     private CredentialsService credentialsService;
 
     @Before
     public void before() {
+        assertThat(mockingDetails(claims).isMock()).isTrue();
+        assertThat(mockingDetails(securityContext).isMock()).isTrue();
+        assertThat(mockingDetails(authentication).isMock()).isTrue();
         assertThat(mockingDetails(credentialsService).isMock()).isTrue();
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        SecurityContextHolder.setContext(securityContext);
+        when(authentication.getPrincipal()).thenReturn(claims);
     }
 
     @Test
