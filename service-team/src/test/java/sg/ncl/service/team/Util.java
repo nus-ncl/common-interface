ee8502e11351e81758e2a2323f98195b1614ffc1
James Ng <jng@ncl.sg>
2016-10-05 11:27:40 +0800

    DEV-646 increase TeamEntity unit test coverage

--- service-team/src/test/java/sg/ncl/service/team/Util.java
+++ service-team/src/test/java/sg/ncl/service/team/Util.java
@@ -3,6 +3,7 @@ package sg.ncl.service.team;
 import org.apache.commons.lang3.RandomStringUtils;
 import sg.ncl.service.team.data.jpa.TeamEntity;
 import sg.ncl.service.team.data.jpa.TeamMemberEntity;
+import sg.ncl.service.team.domain.MemberStatus;
 import sg.ncl.service.team.domain.MemberType;
 import sg.ncl.service.team.web.TeamMemberInfo;
 
@@ -35,13 +36,16 @@ public class Util {
         return entity;
     }
 
-    public static TeamMemberInfo getTeamMemberInfo(MemberType memberType) {
+    public static TeamMemberInfo getTeamMemberInfo(MemberType memberType, MemberStatus... memberStatuses) {
         final Long id = Long.parseLong(RandomStringUtils.randomNumeric(10));
         final TeamMemberEntity teamMemberEntity = new TeamMemberEntity();
         teamMemberEntity.setId(id);
         teamMemberEntity.setUserId(RandomStringUtils.randomAlphanumeric(20));
         teamMemberEntity.setJoinedDate(ZonedDateTime.now());
         teamMemberEntity.setMemberType(memberType);
+        if (memberStatuses.length > 0) {
+            teamMemberEntity.setMemberStatus(memberStatuses[0]);
+        }
         return new TeamMemberInfo(teamMemberEntity);
     }
 }
