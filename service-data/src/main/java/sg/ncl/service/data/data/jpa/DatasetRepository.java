3dd84d3756b29276cc6b5e7c10b27458c01d802a
Zhang Chunwang <zhangchunwang@gmail.com>
2016-10-06 18:35:22 +0800

    DEV-679 Added unit tests for DatasetEntity and DatasetRepositoryEntity

---
+++ service-data/src/main/java/sg/ncl/service/data/data/jpa/DatasetRepository.java
@@ -0,0 +1,9 @@
+package sg.ncl.service.data.data.jpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by dcszwang on 10/6/2016.
+ */
+public interface DatasetRepository extends JpaRepository<DatasetEntity, String> {
+}
