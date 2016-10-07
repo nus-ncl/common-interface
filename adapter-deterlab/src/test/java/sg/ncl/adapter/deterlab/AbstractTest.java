f8ec3b9d3e568a03ef7fd2b41c92437f95e68dd2
teye <a0097953@gmail.com>
2016-10-07 14:45:47 +0800

    DEV-674 remove unused files

--- adapter-deterlab/src/test/java/sg/ncl/adapter/deterlab/AbstractTest.java
+++
@@ -1,9 +0,0 @@
-package sg.ncl.adapter.deterlab;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-/**
- * @author Christopher Zhong
- */
-@SpringBootTest(classes = TestApp.class)
-public abstract class AbstractTest extends sg.ncl.common.test.AbstractTest {}
