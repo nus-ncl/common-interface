d6474c81f17a859ec351cf59c06292f0cc79a420
James Ng <jng@ncl.sg>
2016-10-26 11:58:15 +0800

    DEV-716 Increased test coverage

--- service-data/src/test/java/sg/ncl/service/data/logic/DataServiceImplTest.java
+++ service-data/src/test/java/sg/ncl/service/data/logic/DataServiceImplTest.java
@@ -9,10 +9,16 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.springframework.test.context.TestPropertySource;
+import sg.ncl.common.authentication.Role;
 import sg.ncl.common.exception.base.ForbiddenException;
+import sg.ncl.common.exception.base.NotFoundException;
+import sg.ncl.common.jwt.JwtToken;
 import sg.ncl.service.data.AbstractTest;
 import sg.ncl.service.data.data.jpa.DataEntity;
 import sg.ncl.service.data.data.jpa.DataRepository;
+import sg.ncl.service.data.data.jpa.DataResourceEntity;
+import sg.ncl.service.data.domain.DataAccessibility;
+import sg.ncl.service.data.domain.DataResource;
 import sg.ncl.service.data.domain.DataService;
 import sg.ncl.service.data.domain.DataVisibility;
 import sg.ncl.service.data.exceptions.DataNameInUseException;
@@ -20,6 +26,7 @@ import sg.ncl.service.data.exceptions.DataNotFoundException;
 import sg.ncl.service.data.util.TestUtil;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -106,6 +113,96 @@ public class DataServiceImplTest extends AbstractTest {
         exception.expect(ForbiddenException.class);
 
         dataService.save(dataEntity.getId(), dataEntity, claims);
+    }
+
+    @Test
+    public void testSaveUpdatedDatasetGood() {
+        DataEntity dataEntity = TestUtil.getDataEntity();
+        final List<Role> roles = Collections.singletonList(Role.USER);
+
+        when(dataRepository.getOne(anyLong())).thenReturn(dataEntity);
+        when(claims.get(JwtToken.KEY)).thenReturn(roles);
+        when(claims.getSubject()).thenReturn(dataEntity.getContributorId());
+
+        dataService.save(dataEntity.getId(), dataEntity, claims);
+        verify(dataRepository, times(1)).save(any(DataEntity.class));
+    }
+
+    @Test
+    public void testDeleteDatasetGood() {
+        DataEntity dataEntity = TestUtil.getDataEntity();
+        final List<Role> roles = Collections.singletonList(Role.USER);
+
+        when(dataRepository.getOne(anyLong())).thenReturn(dataEntity);
+        when(claims.get(JwtToken.KEY)).thenReturn(roles);
+        when(claims.getSubject()).thenReturn(dataEntity.getContributorId());
+
+        dataService.delete(dataEntity.getId(), claims);
+        verify(dataRepository, times(1)).delete(anyLong());
+    }
+
+    @Test
+    public void testFindResourceByIdNotAccessible() {
+        DataEntity dataEntity = TestUtil.getDataEntityWithResources();
+        dataEntity.setAccessibility(DataAccessibility.RESTRICTED);
+        final List<Role> roles = Collections.singletonList(Role.USER);
+
+        when(dataRepository.getOne(anyLong())).thenReturn(dataEntity);
+        when(claims.get(JwtToken.KEY)).thenReturn(roles);
+        when(claims.getSubject()).thenReturn("UnknownId");
+
+        exception.expect(ForbiddenException.class);
+        dataService.findResourceById(dataEntity.getId(), dataEntity.getResources().get(0).getId(), claims);
+    }
+
+    @Test
+    public void testFindResourceByIdNotFound() {
+        DataEntity dataEntity = TestUtil.getDataEntity();
+        exception.expect(NotFoundException.class);
+        dataService.findResourceById(dataEntity.getId(), 2L, claims);
+    }
+
+    @Test
+    public void testFindResourceByIdFound() {
+        DataEntity dataEntity = TestUtil.getDataEntity();
+        List<DataResourceEntity> dataResourceList = new ArrayList<>();
+        DataResourceEntity dataResourceEntity = TestUtil.getDataResourceEntity();
+        dataResourceEntity.setId(1L);
+        dataResourceList.add(dataResourceEntity);
+        dataEntity.setResources(dataResourceList);
+
+        when(dataRepository.getOne(anyLong())).thenReturn(dataEntity);
+
+        DataResource dataResource = dataService.findResourceById(dataEntity.getId(), dataEntity.getResources().get(0).getId(), claims);
+        assertThat(dataResource).isEqualTo(dataEntity.getResources().get(0));
+    }
+
+    @Test
+    public void testSaveResource() {
+        DataEntity dataEntity = TestUtil.getDataEntity();
+        DataResourceEntity dataResourceEntity = TestUtil.getDataResourceEntity();
+        final List<Role> roles = Collections.singletonList(Role.USER);
+
+        when(dataRepository.getOne(anyLong())).thenReturn(dataEntity);
+        when(claims.get(JwtToken.KEY)).thenReturn(roles);
+        when(claims.getSubject()).thenReturn(dataEntity.getContributorId());
+
+        dataService.saveResource(dataEntity.getId(), dataResourceEntity, claims);
+        verify(dataRepository, times(1)).save(any(DataEntity.class));
+    }
+
+    @Test
+    public void testDeleteResource() {
+        DataEntity dataEntity = TestUtil.getDataEntity();
+        DataResourceEntity dataResourceEntity = TestUtil.getDataResourceEntity();
+        final List<Role> roles = Collections.singletonList(Role.USER);
+
+        when(dataRepository.getOne(anyLong())).thenReturn(dataEntity);
+        when(claims.get(JwtToken.KEY)).thenReturn(roles);
+        when(claims.getSubject()).thenReturn(dataEntity.getContributorId());
+
+        dataService.deleteResource(dataEntity.getId(), dataResourceEntity.getId(), claims);
+        verify(dataRepository, times(1)).save(any(DataEntity.class));
     }
 
 }
