bc57ccaf0cbfdde66f8213d035e2617af63b6b5e
teye <a0097953@gmail.com>
2016-10-28 17:20:32 +0800

    DEV-758 create service-image

---
+++ service-image/src/main/java/sg/ncl/service/image/web/ImageController.java
@@ -0,0 +1,39 @@
+package sg.ncl.service.image.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import sg.ncl.service.image.domain.ImageService;
+
+import javax.inject.Inject;
+import javax.validation.constraints.NotNull;
+
+import java.util.Map;
+
+import static sg.ncl.service.image.web.ImageController.PATH;
+
+/**
+ * @author Te Ye
+ */
+@RestController
+@RequestMapping(path = PATH, produces = MediaType.APPLICATION_JSON_VALUE)
+@Slf4j
+public class ImageController {
+
+    static final String PATH = "/images";
+
+    private final ImageService imageService;
+
+    @Inject
+    ImageController(@NotNull final ImageService imageService) {
+        this.imageService = imageService;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Map<String, String> getSavedImages() {
+        return null;
+    }
+
+}
