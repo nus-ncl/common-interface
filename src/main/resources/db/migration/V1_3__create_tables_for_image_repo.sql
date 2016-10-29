00653416a9d6556b745ef96fd4a0bc3f7f508b5f
teye <a0097953@gmail.com>
2016-10-29 21:21:59 +0800

    DEV-758 add images table

---
+++ src/main/resources/db/migration/V1_3__create_tables_for_image_repo.sql
@@ -0,0 +1,18 @@
+-- -----------------------------------------------------
+-- Table `prod`.`images`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `prod`.`images` (
+  `id`                 BIGINT(20)   NOT NULL AUTO_INCREMENT,
+  `created_date`       DATETIME     NOT NULL,
+  `last_modified_date` DATETIME     NOT NULL,
+  `version`            BIGINT(20)   NOT NULL,
+  `team_id`            VARCHAR(255) NOT NULL,
+  `node_id`            VARCHAR(255) NOT NULL,
+  `image_name`         VARCHAR(255) NOT NULL,
+  `description`        VARCHAR(255) NULL,
+  `visibility`         VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `UK_krxed372ocm2ni4s42df6hji9` (`id` ASC),
+  INDEX `IDXkrxed372ocm2ni4s42df6hji9` (`id` ASC)
+)
+ENGINE = InnoDB
