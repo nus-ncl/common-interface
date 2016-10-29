c631ff36d230b7a8c93c9de2c61f851d15bad108
teye <a0097953@gmail.com>
2016-10-29 17:48:43 +0800

    DEV-758 add other image structure

---
+++ service-image/src/main/java/sg/ncl/service/image/domain/ImageVisibility.java
@@ -0,0 +1,8 @@
+package sg.ncl.service.image.domain;
+
+/**
+ * @author Te Ye
+ */
+public enum ImageVisibility {
+    PUBLIC, PROTECTED, PRIVATE
+}
--- service-image/src/main/java/sg/ncl/service/image/web/ImageController.java
+++ service-image/src/main/java/sg/ncl/service/image/web/ImageController.java
@@ -4,12 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import sg.ncl.service.image.domain.Image;
 import sg.ncl.service.image.domain.ImageService;
 
 import javax.inject.Inject;
 import javax.validation.constraints.NotNull;
 
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static sg.ncl.service.image.web.ImageController.PATH;
 
@@ -32,20 +35,25 @@ public class ImageController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public Map<String, String> getAll() {
-        return null;
+    public List<Image> getAll() {
+        return imageService.getAll().stream().map(ImageInfo::new).collect(Collectors.toList());
     }
 
     @GetMapping(path = "/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Map<String, String> getImage(@PathVariable final String id) {
-        return null;
+    public Image getImage(@PathVariable final Long id) {
+        return new ImageInfo(imageService.getImage(id));
     }
 
-    // status is ACCEPTED because creating an image is not an instant process
+    /**
+     * Creates a saved image
+     * status is ACCEPTED because creating an image is not an instant process
+     * @param image
+     * @return the created image id
+     */
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public Map<String, String> addImage() {
-        return null;
+    public Long addImage(@RequestBody final ImageInfo image) {
+        return new ImageInfo(imageService.addImage(image)).getId();
     }
 }
