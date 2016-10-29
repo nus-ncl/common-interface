a8d07b8206731e27854464b0284280e212d18107
teye <a0097953@gmail.com>
2016-10-29 19:57:02 +0800

    DEV-758 add image service test

---
+++ service-image/src/test/java/sg/ncl/service/image/logic/ImageServiceTest.java
@@ -0,0 +1,85 @@
+package sg.ncl.service.image.logic;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import sg.ncl.adapter.deterlab.AdapterDeterLab;
+import sg.ncl.service.image.data.jpa.ImageEntity;
+import sg.ncl.service.image.data.jpa.ImageRepository;
+import sg.ncl.service.image.domain.Image;
+import sg.ncl.service.image.domain.ImageService;
+import sg.ncl.service.image.domain.ImageVisibility;
+import sg.ncl.service.image.web.ImageInfo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
+
+import static sg.ncl.service.image.util.TestUtil.getImageEntity;
+
+/**
+ * Created by dcsyeoty on 29-Oct-16.
+ */
+public class ImageServiceTest {
+
+    @Rule
+    public MockitoRule mockito = MockitoJUnit.rule();
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Mock
+    private AdapterDeterLab adapterDeterLab;
+    @Mock
+    private ImageRepository imageRepository;
+
+    private ImageService imageService;
+
+    @Before
+    public void before() {
+        assertThat(mockingDetails(adapterDeterLab).isMock()).isTrue();
+        assertThat(mockingDetails(imageRepository).isMock()).isTrue();
+
+        imageService = new ImageServiceImpl(adapterDeterLab, imageRepository);
+    }
+
+    @Test
+    public void testAddImageGood() {
+        final Image imageInfo = new ImageInfo(1L, "teamId", "imageName", "nodeId", "description", ImageVisibility.PRIVATE);
+
+        when(imageRepository.save(any(ImageEntity.class))).thenAnswer(i -> i.getArgumentAt(0, ImageEntity.class));
+
+        final Image image = imageService.addImage(imageInfo);
+
+        verify(imageRepository, times(1)).save(any(ImageEntity.class));
+        assertThat(image.getTeamId()).isEqualTo(imageInfo.getTeamId());
+        assertThat(image.getImageName()).isEqualTo(imageInfo.getImageName());
+        assertThat(image.getNodeId()).isEqualTo(imageInfo.getNodeId());
+        assertThat(image.getDescription()).isEqualTo(imageInfo.getDescription());
+        assertThat(image.getVisibility()).isEqualTo(imageInfo.getVisibility());
+    }
+
+    @Test
+    public void testGetImageGood() {
+        final ImageEntity entity = getImageEntity();
+
+        when(imageRepository.findOne(eq(entity.getId()))).thenReturn(entity);
+
+        final Image result = imageService.getImage(entity.getId());
+        assertThat(entity.getId()).isEqualTo(result.getId());
+        assertThat(entity.getTeamId()).isEqualTo(result.getTeamId());
+        assertThat(entity.getImageName()).isEqualTo(result.getImageName());
+        assertThat(entity.getNodeId()).isEqualTo(result.getNodeId());
+        assertThat(entity.getDescription()).isEqualTo(result.getDescription());
+        assertThat(entity.getVisibility()).isEqualTo(result.getVisibility());
+    }
+
+    @Test
+    public void testGetListOfImagesGood() {
+
+    }
+
+}
