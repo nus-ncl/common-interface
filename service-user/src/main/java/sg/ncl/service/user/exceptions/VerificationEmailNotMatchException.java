7fad8cf607df6475dbc0dff03991b68eb955f793
Zhang Chunwang <zhangchunwang@gmail.com>
2016-11-17 18:36:02 +0800

    DEV-812 Changed naming of some exceptions for user service

--- service-user/src/main/java/sg/ncl/service/user/exceptions/VerificationEmailNotMatchException.java
+++ service-user/src/main/java/sg/ncl/service/user/exceptions/VerificationEmailNotMatchException.java
@@ -5,9 +5,9 @@ import sg.ncl.common.exception.base.BadRequestException;
 /**
  * Created by dcszwang on 8/12/2016.
  */
-public class EmailNotMatchException extends BadRequestException {
+public class VerificationEmailNotMatchException extends BadRequestException {
 
-    public EmailNotMatchException(final String msg) {
+    public VerificationEmailNotMatchException(final String msg) {
         super(msg);
     }
 }
