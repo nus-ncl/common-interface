342ea3378dcedbb3a30c07cba9d028d1a6a438ff
Zhang Chunwang <zhangchunwang@gmail.com>
2016-11-17 17:15:18 +0800

    DEV-812 Changed naming of exceptions for experiment service

--- service-experiment/src/main/java/sg/ncl/service/experiment/exceptions/ExperimentNotFoundException.java
+++ service-experiment/src/main/java/sg/ncl/service/experiment/exceptions/ExperimentNotFoundException.java
@@ -1,11 +1,12 @@
 package sg.ncl.service.experiment.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import sg.ncl.common.exception.base.NotFoundException;
 
 /**
  * Created by Desmond.
  */
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "There are no experiments.")
-public class ExperimentNotFoundException extends NotFoundException {}
+public class ExperimentNotFoundException extends NotFoundException {
+    public ExperimentNotFoundException(final String message) {
+        super(message);
+    }
+}
