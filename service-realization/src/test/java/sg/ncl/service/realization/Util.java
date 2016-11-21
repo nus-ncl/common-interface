1812b3684635b7e595e56fca8e620da9a1a9bc95
teye <a0097953@gmail.com>
2016-11-21 14:04:33 +0800

    DEV-822 update Util

--- service-realization/src/test/java/sg/ncl/service/realization/Util.java
+++ service-realization/src/test/java/sg/ncl/service/realization/Util.java
@@ -3,6 +3,7 @@ package sg.ncl.service.realization;
 import org.apache.commons.lang3.RandomStringUtils;
 import sg.ncl.service.realization.data.jpa.RealizationEntity;
 import sg.ncl.service.realization.data.jpa.RealizationRepository;
+import sg.ncl.service.realization.domain.RealizationState;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +24,7 @@ public class Util {
         entity.setIdleMinutes(Long.parseLong(RandomStringUtils.randomNumeric(10)));
         entity.setRunningMinutes(Long.parseLong(RandomStringUtils.randomNumeric(10)));
         entity.setDetails(RandomStringUtils.randomAlphanumeric(20));
+        entity.setState(RealizationState.NOT_RUNNING);
         return entity;
     }
 
