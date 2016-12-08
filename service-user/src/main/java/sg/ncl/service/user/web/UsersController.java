7ee13e65635bc8560b48d1901d481496b033f20d
teye <a0097953@gmail.com>
2016-12-08 09:49:53 +0800

    DEV-847 convert users controller to new format

--- service-user/src/main/java/sg/ncl/service/user/web/UsersController.java
+++ service-user/src/main/java/sg/ncl/service/user/web/UsersController.java
@@ -4,41 +4,40 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sg.ncl.service.user.domain.User;
 import sg.ncl.service.user.domain.UserService;
 import sg.ncl.service.user.domain.UserStatus;
 import sg.ncl.service.user.exceptions.UserNotFoundException;
 
 import javax.inject.Inject;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 /**
  * @author Christopher Zhong
  */
 @RestController
+@RequestMapping(path = UsersController.PATH, produces = MediaType.APPLICATION_JSON_VALUE)
 @Slf4j
 public class UsersController {
+
+    static final String PATH = "/users";
 
     private final UserService userService;
 
     @Inject
-    UsersController(final UserService userService) {
+    UsersController(@NotNull final UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping(path = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<User> getAllUsers() {
         return userService.getAll();
     }
 
-    @GetMapping(path = "/users/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public User getUser(@PathVariable String id) {
         User one = userService.getUser(id);
@@ -49,13 +48,13 @@ public class UsersController {
         return new UserInfo(one);
     }
 
-    @PutMapping(path = "/users/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public void updateUser(@PathVariable String id, @RequestBody UserInfo user) {
         userService.updateUser(id, user);
     }
 
-    @PutMapping(path = "/users/{id}/emails/{emailBase64}")
+    @PutMapping(path = "/{id}/emails/{emailBase64}")
     public UserStatus verifyEmail(@PathVariable String id, @PathVariable String emailBase64, @RequestBody VerificationKeyInfo keyInfo) {
         final String email = new String(Base64.decodeBase64(emailBase64));
         return userService.verifyEmail(id, email, keyInfo.getKey());
