ffa665d476ca92ccd51d8a728714c07ebecd4957
James Ng <jng@ncl.sg>
2016-12-16 09:54:39 +0800

    DEV-857 Can only return DataEntity name after deletion

--- service-data/src/main/java/sg/ncl/service/data/web/DataController.java
+++ service-data/src/main/java/sg/ncl/service/data/web/DataController.java
@@ -93,11 +93,11 @@ public class DataController {
     // Delete a data set
     @DeleteMapping(path = "/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Data remove(@AuthenticationPrincipal Object claims, @PathVariable Long id) throws UnsupportedEncodingException {
+    public String remove(@AuthenticationPrincipal Object claims, @PathVariable Long id) throws UnsupportedEncodingException {
         if (claims == null || !(claims instanceof Claims)) {
             throw new UnauthorizedException();
         }
-        return new DataInfo(dataService.deleteDataset(id, (Claims) claims));
+        return String.valueOf(dataService.deleteDataset(id, (Claims) claims).getName());
     }
 
     // View resource in a data set
