d86244c65b300d3fea14d5da27c85bfd1643f561
teye <a0097953@gmail.com>
2016-12-16 17:22:58 +0800

    DEV-859 add get free nodes to controller

---
+++ service-telemetry/src/main/java/sg/ncl/service/telemetry/web/TelemetryController.java
@@ -0,0 +1,43 @@
+package sg.ncl.service.telemetry.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import sg.ncl.service.telemetry.domain.TelemetryService;
+
+import javax.inject.Inject;
+import javax.validation.constraints.NotNull;
+import java.util.HashMap;
+import java.util.Map;
+
+import static sg.ncl.service.telemetry.web.TelemetryController.PATH;
+
+/**
+ * Created by dcsyeoty on 16-Dec-16.
+ */
+@RestController
+@RequestMapping(path = PATH, produces = MediaType.APPLICATION_JSON_VALUE)
+@Slf4j
+public class TelemetryController {
+
+    static final String PATH = "/telemetry";
+
+    private final TelemetryService telemetryService;
+
+    @Inject
+    TelemetryController(@NotNull final TelemetryService telemetryService) {
+        this.telemetryService = telemetryService;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public Map<String, String> getFreeNodes() {
+        Map<String, String> map = new HashMap<>();
+        map.put("numberOfFreeNodes", telemetryService.getFreeNodes());
+        return map;
+    }
+}
