b86b6304980f38a75fe747f5a0a4ee46d67ed078
James Ng <jng@ncl.sg>
2016-12-23 18:06:17 +0800

    DEV-725 Increased test coverage

--- service-data/src/test/java/sg/ncl/service/data/logic/DataAccessRequestServiceImplTest.java
+++ service-data/src/test/java/sg/ncl/service/data/logic/DataAccessRequestServiceImplTest.java
@@ -11,18 +11,30 @@ import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.springframework.test.context.TestPropertySource;
 import sg.ncl.common.DomainProperties;
+import sg.ncl.common.authentication.Role;
+import sg.ncl.common.jwt.JwtToken;
 import sg.ncl.service.data.AbstractTest;
+import sg.ncl.service.data.data.jpa.DataAccessRequestEntity;
 import sg.ncl.service.data.data.jpa.DataAccessRequestRepository;
+import sg.ncl.service.data.data.jpa.DataEntity;
 import sg.ncl.service.data.data.jpa.DataRepository;
 import sg.ncl.service.data.domain.DataAccessRequestService;
+import sg.ncl.service.data.exceptions.DataAccessRequestNotFoundException;
 import sg.ncl.service.data.exceptions.DataNotFoundException;
+import sg.ncl.service.data.util.TestUtil;
 import sg.ncl.service.mail.domain.MailService;
+import sg.ncl.service.user.data.jpa.UserEntity;
 import sg.ncl.service.user.domain.UserService;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyLong;
-import static org.mockito.Mockito.mockingDetails;
-import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by dcsjnh on 12/22/2016.
@@ -75,6 +87,60 @@ public class DataAccessRequestServiceImplTest extends AbstractTest {
         when(dataRepository.getOne(anyLong())).thenReturn(null);
         exception.expect(DataNotFoundException.class);
         dataAccessRequestService.createRequest(1L, "", claims);
+    }
+
+    @Test
+    public void testCreateRequest() throws Exception {
+        DataEntity dataEntity = TestUtil.getDataEntity();
+        UserEntity userEntity = TestUtil.getUserEntity();
+        DataAccessRequestEntity dataAccessRequestEntity = TestUtil.getDataAccessRequestEntity();
+
+        when(dataRepository.getOne(anyLong())).thenReturn(dataEntity);
+        when(userService.getUser(anyString())).thenReturn(userEntity);
+        when(dataAccessRequestRepository.findByDataIdAndRequesterId(anyLong(), anyString())).thenReturn(new ArrayList<DataAccessRequestEntity>());
+        when(dataAccessRequestRepository.save(any(DataAccessRequestEntity.class))).thenReturn(dataAccessRequestEntity);
+
+        dataAccessRequestService.createRequest(1L, "", claims);
+        verify(dataAccessRequestRepository, times(1)).save(any(DataAccessRequestEntity.class));
+    }
+
+    @Test
+    public void testApproveRequestDataNotFound() {
+        when(dataRepository.getOne(anyLong())).thenReturn(null);
+        exception.expect(DataNotFoundException.class);
+        dataAccessRequestService.approveRequest(1L, 1L, claims);
+    }
+
+    @Test
+    public void testApproveRequestDataAccessRequestNotFound() {
+        DataEntity dataEntity = TestUtil.getDataEntity();
+        final List<String> roles = Collections.singletonList(Role.USER.getAuthority());
+
+        when(dataRepository.getOne(anyLong())).thenReturn(dataEntity);
+        when(claims.get(JwtToken.KEY)).thenReturn(roles);
+        when(claims.getSubject()).thenReturn(dataEntity.getContributorId());
+        when(dataAccessRequestRepository.getOne(anyLong())).thenReturn(null);
+
+        exception.expect(DataAccessRequestNotFoundException.class);
+        dataAccessRequestService.approveRequest(1L, 1L, claims);
+    }
+
+    @Test
+    public void testApproveRequest() throws Exception {
+        DataEntity dataEntity = TestUtil.getDataEntity();
+        final List<String> roles = Collections.singletonList(Role.USER.getAuthority());
+        DataAccessRequestEntity dataAccessRequestEntity = TestUtil.getDataAccessRequestEntity();
+        UserEntity userEntity = TestUtil.getUserEntity();
+
+        when(dataRepository.getOne(anyLong())).thenReturn(dataEntity);
+        when(claims.get(JwtToken.KEY)).thenReturn(roles);
+        when(claims.getSubject()).thenReturn(dataEntity.getContributorId());
+        when(dataAccessRequestRepository.getOne(anyLong())).thenReturn(dataAccessRequestEntity);
+        when(dataAccessRequestRepository.save(any(DataAccessRequestEntity.class))).thenReturn(dataAccessRequestEntity);
+        when(userService.getUser(anyString())).thenReturn(userEntity);
+
+        dataAccessRequestService.approveRequest(1L, 1L, claims);
+        verify(dataAccessRequestRepository, times(1)).save(any(DataAccessRequestEntity.class));
     }
 
 }
--- service-data/src/test/java/sg/ncl/service/data/util/TestUtil.java
+++ service-data/src/test/java/sg/ncl/service/data/util/TestUtil.java
@@ -2,11 +2,15 @@ package sg.ncl.service.data.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.RandomStringUtils;
+import sg.ncl.service.data.data.jpa.DataAccessRequestEntity;
 import sg.ncl.service.data.data.jpa.DataEntity;
 import sg.ncl.service.data.data.jpa.DataResourceEntity;
 import sg.ncl.service.data.domain.DataAccessibility;
 import sg.ncl.service.data.domain.DataVisibility;
 import sg.ncl.service.transmission.web.ResumableInfo;
+import sg.ncl.service.user.data.jpa.AddressEntity;
+import sg.ncl.service.user.data.jpa.UserDetailsEntity;
+import sg.ncl.service.user.data.jpa.UserEntity;
 
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
@@ -77,5 +81,45 @@ public class TestUtil {
 
     public static ResumableInfo getResumableInfo() {
         return new ResumableInfo(1, 1L, RandomStringUtils.randomAlphanumeric(20), RandomStringUtils.randomAlphanumeric(20), RandomStringUtils.randomAlphanumeric(20), null);
+    }
+
+    public static DataAccessRequestEntity getDataAccessRequestEntity() {
+        DataAccessRequestEntity entity = new DataAccessRequestEntity();
+        entity.setId(Long.parseLong(RandomStringUtils.randomNumeric(10)));
+        entity.setDataId(Long.parseLong(RandomStringUtils.randomNumeric(10)));
+        entity.setRequesterId(RandomStringUtils.randomAlphanumeric(10));
+        entity.setReason(RandomStringUtils.randomAlphanumeric(20));
+        entity.setRequestDate(ZonedDateTime.now());
+        entity.setApprovedDate(ZonedDateTime.now());
+        return entity;
+    }
+
+    public static UserEntity getUserEntity() throws Exception {
+        final UserEntity userEntity = new UserEntity();
+        userEntity.setApplicationDate(ZonedDateTime.now());
+        userEntity.setVerificationKey(RandomStringUtils.randomAlphanumeric(20));
+
+        final UserDetailsEntity userDetailsEntity = new UserDetailsEntity();
+        userDetailsEntity.setFirstName(RandomStringUtils.randomAlphabetic(20));
+        userDetailsEntity.setLastName(RandomStringUtils.randomAlphabetic(20));
+        userDetailsEntity.setJobTitle(RandomStringUtils.randomAlphabetic(20));
+        userDetailsEntity.setEmail(RandomStringUtils.randomAlphabetic(20));
+        userDetailsEntity.setPhone(RandomStringUtils.randomAlphabetic(20));
+        userDetailsEntity.setInstitution(RandomStringUtils.randomAlphabetic(20));
+        userDetailsEntity.setInstitutionAbbreviation(RandomStringUtils.randomAlphabetic(20));
+        userDetailsEntity.setInstitutionWeb(RandomStringUtils.randomAlphabetic(20));
+
+        final AddressEntity address = new AddressEntity();
+        address.setAddress1(RandomStringUtils.randomAlphabetic(20));
+        address.setAddress2(RandomStringUtils.randomAlphabetic(20));
+        address.setCountry(RandomStringUtils.randomAlphabetic(20));
+        address.setRegion(RandomStringUtils.randomAlphabetic(20));
+        address.setCity(RandomStringUtils.randomAlphabetic(20));
+        address.setZipCode(RandomStringUtils.randomAlphabetic(20));
+
+        userDetailsEntity.setAddress(address);
+        userEntity.setUserDetails(userDetailsEntity);
+
+        return userEntity;
     }
 }
