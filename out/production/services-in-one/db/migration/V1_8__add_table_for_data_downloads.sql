e618f181e54afde892804e15bc64bc680660bc0a
Tran Ly Vu <vutransingapore@gmail.com>
2016-12-27 10:57:19 +0800

    update unit test

--- out/production/services-in-one/db/migration/V1_8__add_table_for_data_downloads.sql
+++ out/production/services-in-one/db/migration/V1_8__add_table_for_data_downloads.sql
@@ -3,12 +3,16 @@
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `prod`.`data_downloads` (
   `id`              BIGINT(20)   NOT NULL AUTO_INCREMENT,
+  `created_date`       DATETIME     NOT NULL,
+  `last_modified_date` DATETIME     NOT NULL,
+  `version`            BIGINT(20)   NOT NULL,
   `data_id`         BIGINT(20)   NOT NULL,
   `resource_id`     BIGINT(20)   NOT NULL,
   `download_date`   DATETIME     NOT NULL,
   `hashed_user_id`  VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `UK_m23m239poytcx576ugujgrr` (`hashed_user_id` ASC)
+  FOREIGN KEY (`data_id`) REFERENCES `prod`.`data` (`id`),
+  FOREIGN KEY (`resource_id`) REFERENCES `prod`.`data_resources` (`id`)
 )
   ENGINE = InnoDB
   /*!  DEFAULT CHARACTER SET = latin1 */;
