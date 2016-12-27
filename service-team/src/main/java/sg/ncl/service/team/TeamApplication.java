04eb857300a8f999f8ec3dead0a88bed197201ad
teye <a0097953@gmail.com>
2016-12-27 17:44:00 +0800

    DEV-858 import dependencies

--- service-team/src/main/java/sg/ncl/service/team/TeamApplication.java
+++ service-team/src/main/java/sg/ncl/service/team/TeamApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Import;
+import sg.ncl.adapter.deterlab.DeterLabAutoConfiguration;
 import sg.ncl.common.authentication.AuthenticationAutoConfiguration;
 import sg.ncl.common.exception.ExceptionAutoConfiguration;
 import sg.ncl.common.jpa.UseJpa;
 import sg.ncl.common.jwt.JwtAutoConfiguration;
+import sg.ncl.service.user.UserApplication;
 
 /**
  * @author Christopher Zhong
@@ -16,8 +18,10 @@ import sg.ncl.common.jwt.JwtAutoConfiguration;
 @UseJpa
 @Import({
         AuthenticationAutoConfiguration.class,
+        DeterLabAutoConfiguration.class,
         ExceptionAutoConfiguration.class,
-        JwtAutoConfiguration.class
+        JwtAutoConfiguration.class,
+        UserApplication.class
 })
 public class TeamApplication {
 
