0c192cc575eee6be3de6314a8c4f0c3f69119151
Tran Ly Vu <vutransingapore@gmail.com>
2016-12-27 17:33:00 +0800

    fix based on comments

--- src/main/java/sg/ncl/service/ServicesInOneApplication.java
+++ src/main/java/sg/ncl/service/ServicesInOneApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.system.ApplicationPidFileWriter;
 import org.springframework.context.annotation.Import;
-import sg.ncl.service.analytics.AnalyticsApplication;
 import sg.ncl.service.authentication.AuthenticationApplication;
 import sg.ncl.service.data.DataApplication;
 import sg.ncl.service.experiment.ExperimentApplication;
@@ -32,8 +31,7 @@ import java.sql.SQLException;
         RegistrationApplication.class,
         TeamApplication.class,
         UserApplication.class,
-        VersionApplication.class,
-        AnalyticsApplication.class
+        VersionApplication.class
 })
 public class ServicesInOneApplication {
 
