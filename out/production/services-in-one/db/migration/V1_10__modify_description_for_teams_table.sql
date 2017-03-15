6a30c584bc4469ed4faf3fb80e79c4092b55a116
Tran Ly Vu <vutransingapore@gmail.com>
2017-03-15 10:20:36 +0800

    .

--- out/production/services-in-one/db/migration/V1_10__modify_description_for_teams_table.sql
+++
@@ -1,6 +0,0 @@
--- -----------------------------------------------------
--- AlTER `teams`.`description`
--- -----------------------------------------------------
-USE `prod`;
-ALTER TABLE teams
-MODIFY COLUMN description LONGTEXT;
