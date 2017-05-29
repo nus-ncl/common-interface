7884fc2014f8a8644d9ffaad447f46cdab9c4a14
teye <a0097953@gmail.com>
2017-05-29 16:23:00 +0800

    add av-scanner properties

---
+++ common/src/main/java/sg/ncl/common/AvScannerProperties.java
@@ -0,0 +1,17 @@
+package sg.ncl.common;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @Author teye
+ */
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "ncl.av-scanner")
+public class AvScannerProperties {
+    private String host;
+    private Integer port;
+}
