a7041d02662f8ac42e219af3d381bb46bd603571
teye <a0097953@gmail.com>
2017-05-30 12:18:28 +0800

    add is_malicious column for data resource

--- service-data/src/main/java/sg/ncl/service/data/web/DataResourceInfo.java
+++ service-data/src/main/java/sg/ncl/service/data/web/DataResourceInfo.java
@@ -13,18 +13,21 @@ public class DataResourceInfo implements DataResource {
 
     private Long id;
     private String uri;
+    private boolean malicious;
 
     @JsonCreator
     public DataResourceInfo(
             @JsonProperty("id") final Long id,
-            @JsonProperty("uri") final String uri
+            @JsonProperty("uri") final String uri,
+            @JsonProperty("is_malicious") final boolean malicious
     ) {
         this.id = id;
         this.uri = uri;
+        this.malicious = malicious;
     }
 
     public DataResourceInfo(final DataResource dataResource) {
-        this(dataResource.getId(), dataResource.getUri());
+        this(dataResource.getId(), dataResource.getUri(), dataResource.isMalicious());
     }
 
 }
