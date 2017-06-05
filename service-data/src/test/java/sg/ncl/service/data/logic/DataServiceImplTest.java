13abd7ba683e1df855d64c2441b7777530275bda
teye <a0097953@gmail.com>
2017-06-05 16:05:38 +0800

    fix tests for async scanner service

--- service-data/src/test/java/sg/ncl/service/data/logic/DataServiceImplTest.java
+++ service-data/src/test/java/sg/ncl/service/data/logic/DataServiceImplTest.java
@@ -64,7 +64,7 @@ public class DataServiceImplTest {
     @Mock
     private AnalyticsService analyticsService;
     @Mock
-    private AvScannerService avScannerService;
+    private AsyncAvScannerService asyncAvScannerService;
     @Mock
     private Claims claims;
     @Mock
@@ -80,8 +80,8 @@ public class DataServiceImplTest {
         assertThat(mockingDetails(uploadService).isMock()).isTrue();
         assertThat(mockingDetails(downloadService).isMock()).isTrue();
         assertThat(mockingDetails(analyticsService).isMock()).isTrue();
-        assertThat(mockingDetails(avScannerService).isMock()).isTrue();
-        dataService = new DataServiceImpl(dataRepository, dataCategoryRepository, dataLicenseRepository, uploadService, downloadService, analyticsService, avScannerService);
+        assertThat(mockingDetails(asyncAvScannerService).isMock()).isTrue();
+        dataService = new DataServiceImpl(dataRepository, dataCategoryRepository, dataLicenseRepository, uploadService, downloadService, analyticsService, asyncAvScannerService);
     }
 
     @Test
