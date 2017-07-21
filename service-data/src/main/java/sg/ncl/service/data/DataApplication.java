12a23ff7a42893b8f906ce1deb38a95b87556503
teye <a0097953@gmail.com>
2017-07-21 17:58:56 +0800

    remove unused imports

--- service-data/src/main/java/sg/ncl/service/data/DataApplication.java
+++ service-data/src/main/java/sg/ncl/service/data/DataApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import sg.ncl.common.jpa.UseJpa;
 
 import java.io.IOException;
