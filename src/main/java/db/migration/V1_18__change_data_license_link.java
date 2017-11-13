316f32bb8053fc675199295ab4e6b9f47a2fc700
Zhang Chunwang <zhangchunwang@gmail.com>
2017-11-13 16:54:04 +0800

    Try to triger a new build to solve sonarqube error

--- src/main/java/db/migration/V1_18__change_data_license_link.java
+++ src/main/java/db/migration/V1_18__change_data_license_link.java
@@ -12,14 +12,14 @@ public class V1_18__change_data_license_link implements SpringJdbcMigration {
 
     @Override
     public void migrate(final JdbcTemplate jdbcTemplate) throws Exception {
-        log.info("Setting public domain license link.");
+        log.debug("Setting public domain license link.");
         updateLicense(jdbcTemplate, "CC0", "https://creativecommons.org/publicdomain/zero/1.0/");
     }
 
     private void updateLicense(final JdbcTemplate jdbcTemplate, final String acronym, final String link) {
         final String sql = "UPDATE prod.data_licenses SET link=? WHERE acronym=?";
         final int i = jdbcTemplate.update(sql, link, acronym);
-        log.info("Number of rows affected: {}", i);
+        log.debug("Number of rows affected: {}", i);
     }
 
 }
