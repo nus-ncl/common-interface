2b862f3331e80d22e6c217ae342fa33445162765
Tran Ly Vu <vutransingapore@gmail.com>
2018-01-12 17:00:28 +0800

    Add OpenStack connection exception
    Signed-off-by: Tran Ly Vu <vutransingapore@gmail.com>

--- adapter-openstack/src/main/java/sg/ncl/adapter/openstack/exceptions/OpenStackConnectionException.java
+++ adapter-openstack/src/main/java/sg/ncl/adapter/openstack/exceptions/OpenStackConnectionException.java
@@ -1,9 +1,19 @@
 package sg.ncl.adapter.openstack.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * Author: Tran Ly Vu
  */
-public class OpenStackConnectionException {
+@ResponseStatus(value = HttpStatus.REQUEST_TIMEOUT, reason = "Connection refused to OpenStack controller")
+public class OpenStackConnectionException extends RuntimeException{
 
+    public OpenStackConnectionException() {
+        super("Connection refused to OpenStack controller");
+    }
 
+    public OpenStackConnectionException(final String message) {
+        super(message);
+    }
 }
