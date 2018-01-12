5e68424c42c0a754e605361237fffe5b4a286cb1
Tran Ly Vu <vutransingapore@gmail.com>
2018-01-12 17:35:15 +0800

    Update create user function in openstack adapter module
    Signed-off-by: Tran Ly Vu <vutransingapore@gmail.com>

--- adapter-openstack/src/main/java/sg/ncl/adapter/openstack/AdapterOpenStack.java
+++ adapter-openstack/src/main/java/sg/ncl/adapter/openstack/AdapterOpenStack.java
@@ -4,6 +4,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
@@ -19,14 +20,16 @@ import javax.management.openmbean.OpenDataException;
 @Slf4j
 public class AdapterOpenStack {
 
-    @Autowired
-    protected RestTemplate restTemplate;
+    private RestTemplate restTemplate;
+    private ConnectionProperties properties;
 
     @Inject
-    protected ConnectionProperties properties;
+    public AdapterOpenStack(ConnectionProperties properties, RestTemplate restTemplate) {
+        this.properties = properties;
+        this.restTemplate = restTemplate;
+    }
 
-
-    public void requestToken (String name, String password) {
+    public JSONObject requestToken (String name, String password) {
         JSONObject domainObject = new JSONObject();
         domainObject.put("name", "Default");
 
@@ -64,6 +67,8 @@ public class AdapterOpenStack {
         }
 
         JSONObject token = new JSONObject(response.getBody().toString());
+
+        return token;
     }
 
     public JSONObject createUser(String name, String password) {
@@ -71,6 +76,19 @@ public class AdapterOpenStack {
         userObject.put("enabled", true);
         userObject.put("name", name);
         userObject.put("password", password);
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("user", userObject);
+
+        JSONObject token = requestToken(name, password);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        httpHeaders.set("X-Auth-Token", token.toString());
+        HttpEntity<String> request = new HttpEntity<>(jsonObject.toString(), httpHeaders);
+        ResponseEntity responseEntity;
+
+        
 
         return new JSONObject();
 
