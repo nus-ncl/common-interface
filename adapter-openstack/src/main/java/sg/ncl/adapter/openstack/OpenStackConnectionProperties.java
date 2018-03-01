8126a007003f50bdfa5365e260ab3905674fa77f
Tran Ly Vu <vutransingapore@gmail.com>
2018-03-01 13:13:45 +0800

    Change enable filed
    Signed-off-by: Tran Ly Vu <vutransingapore@gmail.com>

--- adapter-openstack/src/main/java/sg/ncl/adapter/openstack/OpenStackConnectionProperties.java
+++ adapter-openstack/src/main/java/sg/ncl/adapter/openstack/OpenStackConnectionProperties.java
@@ -23,7 +23,7 @@ public class OpenStackConnectionProperties {
     private String adminPass;
     private String adminId;
     private String adminProjectId;
-    private boolean enabled = true;
+    private boolean enabled = false;
 
     public String requestTokenUrl() {
         return HTTP_MODE + ip + ":" + identityPort + "/v3/auth/tokens";
