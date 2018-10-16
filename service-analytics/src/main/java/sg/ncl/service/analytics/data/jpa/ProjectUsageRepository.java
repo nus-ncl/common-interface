1fb92b9c8902876ca5946f8b8374c8d356d819a1
James Ng <jng@ncl.sg>
2018-10-16 10:22:28 +0800

    Remove reduntant class interface

--- service-analytics/src/main/java/sg/ncl/service/analytics/data/jpa/ProjectUsageRepository.java
+++
@@ -1,7 +0,0 @@
-package sg.ncl.service.analytics.data.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectUsageRepository extends JpaRepository<ProjectUsageEntity, ProjectUsageIdentity> {
-
-}
