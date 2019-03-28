f85fe2b6c5e6474cd69e6ec4bfe407c496bc16fa
James Ng <jng@ncl.sg>
2019-03-28 16:06:06 +0800

    Fix overlapping query

--- service-analytics/src/main/java/sg/ncl/service/analytics/data/jpa/NodesReservationRepository.java
+++ service-analytics/src/main/java/sg/ncl/service/analytics/data/jpa/NodesReservationRepository.java
@@ -10,14 +10,14 @@ import java.util.List;
 public interface NodesReservationRepository extends JpaRepository<NodesReservationEntity, Long> {
 
     @Query(value = "SELECT CASE WHEN count(n) > 0 THEN true ELSE false END FROM NodesReservationEntity n " +
-                   "WHERE n.projectId = :projectId AND NOT (n.startDate >= :endDate OR n.endDate <= :startDate)")
+                   "WHERE n.projectId = :projectId AND NOT (n.startDate > :endDate OR n.endDate < :startDate)")
     boolean existsByOverlappedDates(@Param("projectId") Long projectId,
                                     @Param("startDate") ZonedDateTime startDate,
                                     @Param("endDate") ZonedDateTime endDate);
 
     @Query(value = "SELECT NEW sg.ncl.service.analytics.data.jpa.NodesReservationEntry(p.projectName, n.startDate, n.endDate, n.noNodes) " +
                     "FROM NodesReservationEntity n, ProjectDetailsEntity p WHERE n.projectId = p.id " +
-                    "AND NOT (n.startDate >= :endDate OR n.endDate <= :startDate)")
+                    "AND NOT (n.startDate > :endDate OR n.endDate < :startDate)")
     List<NodesReservationEntry> findNodesReservationOverlappedDates(@Param("startDate") ZonedDateTime startDate,
                                                                     @Param("endDate") ZonedDateTime endDate);
 }
