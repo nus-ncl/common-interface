656eee1dc3c42751dd1b1e1cc5f71bcc54670538
hkwany <hkwany520@gmail.com>
2021-03-15 20:57:36 +0800

    updated jdbcTemplate.java

--- src/main/java/db/migration/DataMigration_jdbcTemplate.java
+++ src/main/java/db/migration/DataMigration_jdbcTemplate.java
@@ -11,26 +11,40 @@ import org.apache.ibatis.jdbc.ScriptRunner;
 
 public class DataMigration_jdbcTemplate {
     public static void main(String[] args) throws Exception {
+        boolean IMPORT_SQL = true;
+        boolean RUN_JAVA = true;
         String mysqlUrl = "jdbc:mysql://localhost:3306";
         Connection con = DriverManager.getConnection(mysqlUrl, "root", "root");
         System.out.println("Connection Established...");
         ScriptRunner scriptRunner = new ScriptRunner(con);
 
-        File sql_dir = new File("/home/localuser/IdeaProjects/services-in-one/src/main/resources/db/migration");
-        File java_dir = new File("/home/localuser/IdeaProjects/services-in-one/src/main/java/db/migration");
-        File[] sql_files = sql_dir.listFiles();
-        File[] java_files = java_dir.listFiles();
-        if (sql_files != null){
-            for (File child : sql_files){
+        String sql_dir = "/home/hkwany/IdeaProjects/services-in-one/src/main/resources/db/migration/";
+        String[] sql_files = {"V1_0__initial_schema.sql", "V1_2__create_tables_for_data_repo.sql",
+                "V1_3__create_tables_for_image_repo.sql",
+                "V1_5__add_table_for_password_reset_request.sql",
+                "V1_8__add_table_for_data_downloads.sql",
+                "V1_9__add_table_for_data_access_request.sql",
+                "V1_10__modify_description_for_teams_table.sql",
+                "V1_11__add_table_for_team_quotas.sql",
+                "V1_12__create_tables_for_catalogue.sql",
+                "V1_14__create_table_for_data_licenses.sql",
+                "V1_16__alter_table_for_data_resources_scanning.sql",
+                "V1_17__create_tables_for_public_user_downloads.sql",
+                "V1_20__alter_table_for_teams.sql",
+                "V1_21__create_tables_for_project_monthly_usage.sql",
+                "V1_22__create_table_for_nodes_usage_reservations.sql",
+                "V1_23__alter_table_for_project_usage.sql"};
+        if(IMPORT_SQL) {
+            for (int i = 1; i < sql_files.length; i++) {
                 System.out.println("Import SQL Starting......");
-                System.out.println(child.getAbsolutePath());
-                Reader reader = new BufferedReader(new FileReader(child.getAbsolutePath()));
+                Reader reader = new BufferedReader(new FileReader(sql_dir + sql_files[i]));
                 scriptRunner.runScript(reader);
                 System.out.println("Import SQL Finished......");
+
             }
             System.out.println("Import SQL All Finished......");
         }
-        if (java_files != null) {
+        if (RUN_JAVA) {
             ApplicationContext ctx = new ClassPathXmlApplicationContext("application.xml");
             JdbcTemplate jdbcTemplate = (JdbcTemplate) ctx.getBean("jdbcTemplate");
             System.out.println("Run Java Starting......");
